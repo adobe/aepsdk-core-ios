b737b1375990a56a5736e50e4c73d7dd0081ac99
Nick Porter <43650450+nporter-adbe@users.noreply.github.com>
2020-06-11 11:12:50 -0600

    [AMSDK-10026] Stub Identity public API (#20)
    * start on identity public API
    
    * Start creating the Identity extension
    
    * Implement Identity public API
    
    * Fill in IdentityConstants with values
    
    * Add doc comments to Identity protocol
    
    * Add doc comment for buildIdSyncEvent
    
    * Add tests for Identity public API
    
    * Use absoloute string instead of absolute url
    
    * Only build event once and chain APIs
    
    * Make MobileVisitorAuthenticationState obj-c compatible
    
    * Make MobileVisitorId Obj-C compatible and extract definition out into a protocol
    
    * Pass auth state into syncIdentifiers from syncIdentifier
    
    * Have APIs return VisitorId instead of MobileVisitorId
    
    * Update naming conventions to remove the use of "visitor"
    
    * Rename visitorId to Identifiable
    
    * Remove overloaded APIs in favor of APIs with errors. We will handle non-error APIs in the shim layer.
    
    * Update APIs given the timeout changes to response listeners

---
+++ identity/IdentityConstants.swift
@@ -0,0 +1,28 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import Foundation
+
+struct IdentityConstants {
+    static let API_TIMEOUT = TimeInterval(0.5) // Get API requests timeout after half a second
+    
+    struct EventDataKeys {
+        static let BASE_URL = "baseurl"
+        static let UPDATED_URL = "updatedurl"
+        static let VISITOR_IDS_LIST = "visitoridslist"
+        static let VISITOR_ID_MID = "mid"
+        static let IDENTIFIERS = "visitoridentifiers"
+        static let AUTHENTICATION_STATE = "authenticationstate"
+        static let FORCE_SYNC = "forcesync"
+        static let IS_SYNC_EVENT = "issyncevent"
+        static let URL_VARIABLES = "urlvariables"
+    }
+}
