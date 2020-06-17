4d27face6834fbd5612aa1d462c50fa27ee1f0b7
Nick Porter <43650450+nporter-adbe@users.noreply.github.com>
2020-06-16 23:58:06 -0600

    [AMSDK-10206] Identity Hit URL Building (#41)
    * add tests
    
    * Add networking constants for identity
    
    * Add url encoder
    
    * Start writing tests for identity extensions
    
    * refact extensions into their own files
    
    * Add some more tests for building identity url
    
    * Add tests for multiple custom ids and dpids
    
    * Add doc comment for dpid query item init
    
    * Remove un used file
    
    * Remove dangling comment
    
    * Improve naming
    
    * Fix URLEncoder test
    
    * Only add delimiter if ID is non-empty
    
    * Rename URL creation functions to be more clear
    
    * Fix dpid test
    
    * Fix test (can't guarantee order of parameters)
    
    * Add log todo for failure to create URL

--- Sources/identity/IdentityConstants.swift
+++ Sources/identity/IdentityConstants.swift
@@ -14,6 +14,14 @@ import Foundation
 struct IdentityConstants {
     static let API_TIMEOUT = TimeInterval(0.5) // Get API requests timeout after half a second
     static let DEFAULT_TTL = TimeInterval(600)
+    static let RESPONSE_KEY_ORGID = "d_orgid"
+    static let RESPONSE_KEY_MID = "d_mid"
+    static let RESPONSE_KEY_BLOB = "d_blob"
+    static let RESPONSE_KEY_HINT = "dcs_region"
+    static let VISITOR_ID_PARAMETER_KEY_CUSTOMER = "d_cid_ic"
+    static let KEY_PATH_OPTOUT = "/demoptout.jpg"
+    static let DEFAULT_SERVER = "dpm.demdex.net"
+    static let CID_DELIMITER = "%01"
     
     struct EventDataKeys {
         static let BASE_URL = "baseurl"
