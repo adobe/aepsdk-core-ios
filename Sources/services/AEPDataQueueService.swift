32ea1f330f643cf2da5df89ffa1d75cb7731d540
Yansong Yang <yanyang@adobe.com>
2020-06-24 13:37:15 -0600

    Add DataQueueService to Core (#38)
    * create a new platform service =>  DataQueueService
    
    * Update README.md
    
    * response to review comments
    
    * response to review feedbacks
    
    * add missing comments
    
    * update a property name

---
+++ Sources/services/AEPDataQueueService.swift
@@ -0,0 +1,38 @@
+/*
+ Copyright 2020 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import Foundation
+
+/// An implementation of protocol `DataQueueService`
+///      - initializes `DataQueue` objects by label name
+///      - caches `DataQueue` objects, then it can be retrieved later by the same label name
+public class AEPDataQueueService: DataQueueService {
+    public static let shared: DataQueueService = AEPDataQueueService()
+    private let serialQueue = DispatchQueue(label: "com.adobe.marketing.mobile.dataqueueservice")
+    #if DEBUG
+    internal var threadSafeDictionary = ThreadSafeDictionary<String, DataQueue>()
+    #else
+    private var threadSafeDictionary = ThreadSafeDictionary<String, DataQueue>()
+    #endif
+
+    private init() {}
+
+    public func getDataQueue(label databaseName: String) -> DataQueue? {
+        if let queue = threadSafeDictionary[databaseName] {
+            return queue
+        } else {
+            let dataQueue = AEPDataQueue(databaseName: databaseName, serialQueue: serialQueue)
+            threadSafeDictionary[databaseName] = dataQueue
+            return dataQueue
+        }
+    }
+}
