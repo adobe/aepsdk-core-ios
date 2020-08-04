b68e6629858e722828013410845887967fbb95d6
Nick Porter <nporter@adobe.com>
2020-08-04 14:44:02 -0600

    Implement more bridge APIs

--- BackwardsCompat/ACPCore.m
+++ BackwardsCompat/ACPCore.m
@@ -23,7 +23,7 @@ governing permissions and limitations under the License.
 #pragma mark - Configuration
 
 + (void) configureWithAppId: (NSString* __nullable) appid {
-    [AEPCore configureWithAppId:appid]'
+    [AEPCore configureWithAppId:appid];
 }
 
 + (void) configureWithFileInPath: (NSString* __nullable) filepath {
@@ -32,7 +32,7 @@ governing permissions and limitations under the License.
 
 + (void) getSdkIdentities: (nullable void (^) (NSString* __nullable content)) callback {
     [AEPCore getSdkIdentities:^(NSString * _Nullable content, enum AEPError error) {
-        callback(content)
+        callback(content);
     }];
 }
 
@@ -41,11 +41,15 @@ governing permissions and limitations under the License.
 }
 
 + (void) getPrivacyStatus: (nonnull void (^) (ACPMobilePrivacyStatus status)) callback {
-
+    [AEPCore getPrivacyStatus:^(enum AEPPrivacyStatus status) {
+        callback([self convertToACPPrivacyStatus:status]);
+    }];
 }
 
 + (void) getPrivacyStatusWithCompletionHandler: (nonnull void (^) (ACPMobilePrivacyStatus status, NSError* _Nullable error)) callback {
-    
+    [AEPCore getPrivacyStatus:^(enum AEPPrivacyStatus status) {
+        callback([self convertToACPPrivacyStatus:status], nil);
+    }];
 }
 
 
@@ -58,11 +62,11 @@ governing permissions and limitations under the License.
 }
 
 + (void) setLogLevel: (ACPMobileLogLevel) logLevel {
-    
+    [AEPCore setLogLevel:[self convertToAEPLogLevel:logLevel]];
 }
 
 + (void) setPrivacyStatus: (ACPMobilePrivacyStatus) status {
-    
+    [AEPCore setPrivacy:[self convertToAEPPrivacyStatus:status]];
 }
 
 + (void) updateConfiguration: (NSDictionary* __nullable) config {
@@ -144,11 +148,11 @@ governing permissions and limitations under the License.
 #pragma mark - Logging Utilities
 
 + (ACPMobileLogLevel) logLevel {
-    // TODO
-    return ACPMobileLogLevelDebug;
+    return [self convertToACPLogLevel:[AEPLog logFilter]];
 }
 
 + (void) log: (ACPMobileLogLevel) logLevel tag: (nonnull NSString*) tag message: (nonnull NSString*) message {
+    // TODO:
 }
 
 #pragma mark - Rules Engine
@@ -160,31 +164,119 @@ governing permissions and limitations under the License.
 #pragma mark - Wrapper Support
 
 + (void) setWrapperType: (ACPMobileWrapperType) wrapperType {
-    [AEPCore setWrapperType:[self convertWrapperType:wrapperType]];
+    [AEPCore setWrapperType:[self covertToAEPWrapperType:wrapperType]];
 }
 
-+ (AEPWrapperType) convertWrapperType: (ACPMobileWrapperType) wrapperType {
++ (ACPMobilePrivacyStatus)convertToACPPrivacyStatus: (AEPPrivacyStatus) privacyStatus {
+    switch (privacyStatus) {
+        case AEPPrivacyStatusOptedIn:
+            return ACPMobilePrivacyStatusOptIn;
+            break;
+        case AEPPrivacyStatusOptedOut:
+            return ACPMobilePrivacyStatusOptOut;
+            break;
+        default:
+            return ACPMobilePrivacyStatusUnknown;
+            break;
+    }
+}
+
++ (AEPPrivacyStatus)convertToAEPPrivacyStatus: (ACPMobilePrivacyStatus) privacyStatus {
+    switch (privacyStatus) {
+        case ACPMobilePrivacyStatusOptIn:
+            return AEPPrivacyStatusOptedIn;
+            break;
+        case ACPMobilePrivacyStatusOptOut:
+            return AEPPrivacyStatusOptedOut;
+            break;
+        default:
+            return AEPPrivacyStatusUnknown;
+            break;
+    }
+}
+
++ (ACPMobileWrapperType)covertToACPWrapperType: (AEPWrapperType) wrapperType {
+    switch (wrapperType) {
+        case AEPWrapperTypeNone:
+            return ACPMobileWrapperTypeNone;
+            break;
+        case AEPWrapperTypeReactNative:
+            return ACPMobileWrapperTypeReactNative;
+            break;
+        case AEPWrapperTypeFlutter:
+            return ACPMobileWrapperTypeFlutter;
+            break;
+        case AEPWrapperTypeCordova:
+            return ACPMobileWrapperTypeCordova;
+            break;
+        case AEPWrapperTypeUnity:
+            return ACPMobileWrapperTypeUnity;
+            break;
+        case AEPWrapperTypeXamarin:
+            return ACPMobileWrapperTypeXamarin;
+            break;
+        default:
+            return ACPMobileWrapperTypeNone;
+            break;
+    }
+}
+
++ (AEPWrapperType)covertToAEPWrapperType: (ACPMobileWrapperType) wrapperType {
     switch (wrapperType) {
         case ACPMobileWrapperTypeNone:
-            return AEPWrapperTypeNone
+            return AEPWrapperTypeNone;
             break;
         case ACPMobileWrapperTypeReactNative:
-            return AEPWrapperTypeReactNative
+            return AEPWrapperTypeReactNative;
             break;
         case ACPMobileWrapperTypeFlutter:
-            return AEPWrapperTypeFlutter
+            return AEPWrapperTypeFlutter;
             break;
         case ACPMobileWrapperTypeCordova:
-            return AEPWrapperTypeCordova
+            return AEPWrapperTypeCordova;
             break;
         case ACPMobileWrapperTypeUnity:
-            return AEPWrapperTypeUnity
+            return AEPWrapperTypeUnity;
             break;
         case ACPMobileWrapperTypeXamarin:
-            return AEPWrapperTypeXamarin
+            return AEPWrapperTypeXamarin;
             break;
         default:
             return AEPWrapperTypeNone;
+            break;
+    }
+}
+
++ (AEPLogLevel)convertToAEPLogLevel: (ACPMobileLogLevel) logLevel {
+    switch (logLevel) {
+        case ACPMobileLogLevelDebug:
+            return AEPLogLevelDebug;
+            break;
+        case ACPMobileLogLevelWarning:
+            return AEPLogLevelWarning;
+            break;
+        case ACPMobileLogLevelError:
+            return AEPLogLevelError;
+            break;
+        default:
+            return AEPLogLevelError;
+            break;
+    }
+}
+
++ (ACPMobileLogLevel)convertToACPLogLevel: (AEPLogLevel) logLevel {
+    switch (logLevel) {
+        case AEPLogLevelDebug:
+            return ACPMobileLogLevelDebug;
+            break;
+        case AEPLogLevelWarning:
+            return ACPMobileLogLevelWarning;
+            break;
+        case AEPLogLevelError:
+            return ACPMobileLogLevelError;
+            break;
+        default:
+            return ACPMobileLogLevelError;
             break;
     }
 }
