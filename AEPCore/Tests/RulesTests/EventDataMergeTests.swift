abf9068c124e22f1d2e91ba2e79f30580317a0bd
jgeng <jgeng@adobe.com>
2020-08-06 13:03:31 -0600

    add some more tests

--- AEPCore/Tests/RulesTests/EventDataMergeTests.swift
+++ AEPCore/Tests/RulesTests/EventDataMergeTests.swift
@@ -14,6 +14,23 @@ import XCTest
 @testable import AEPCore
 @testable import AEPServices
 
+
+extension Dictionary where Key == String, Value == Any {
+    func flattening(prefix: String = "") -> [String: Any] {
+        let keyPrefix = (prefix.count > 0) ? (prefix + ".") : prefix
+        var flattenedDict = [String: Any]()
+        for (key, value) in self {
+            let expandedKey = keyPrefix + key
+            if let dict = value as? [String: Any] {
+                flattenedDict.merge(dict.flattening(prefix: expandedKey)) { _, new in new }
+            } else {
+                flattenedDict[expandedKey] = value
+            }
+        }
+        return flattenedDict
+    }
+}
+
 class EventDataMergeTests: XCTestCase {
      
     
@@ -476,6 +493,156 @@ class EventDataMergeTests: XCTestCase {
         XCTAssertEqual("newValue", array[1]["key"] as? String)
         XCTAssertEqual("item2", array[1]["item2"] as? String)
     }
+    
+    
+    
+    func testArrayWildCardInnerMapMergeOverwrite(){
+        
+        let toData = """
+        {
+            "array": [
+                {
+                    "item1": "item1",
+                    "key": "oldValue",
+                    "inner" : {
+                        "innerKey": "oldValue",
+                        "notToMerge": "oldValue"
+
+                    }
+                },
+                {
+                    "item2": "item2"
+                }
+            ]
+        }
+        """.data(using: .utf8)!
+        
+        
+        let fromData = """
+        {
+            "array[*]": {
+                    "key": "newValue",
+                    "inner" : {
+                        "innerKey": "newValue",
+                        "newKey": "newValue",
+                    }
+                }
+        }
+        """.data(using: .utf8)!
+        
+        let toDict = try! JSONDecoder().decode(AnyCodable.self, from: toData).dictionaryValue!
+        let fromDict = try! JSONDecoder().decode(AnyCodable.self, from: fromData).dictionaryValue!
+        
+        let result = EventDataMerger.merging(to: toDict, from: fromDict, overwrite: true)
+        
+        /*
+          {
+                "array": [
+                    {
+                        "item1": "item1",
+                        "key": "newValue",
+                        "inner" : {
+                            "innerKey": "newValue",
+                            "newKey": "newValue",
+                            "notToMerge": "oldValue"
+
+                        }
+                    },
+                    {
+                        "item2": "item2",
+                        "key": "newValue",
+                         "inner" : {
+                             "innerKey": "newValue",
+                             "newKey": "newValue"
+
+                         }
+                    }
+                ]
+            }
+        */
+        let array =  result["array"] as! [[String:Any]]
+        let innerDict1 = array[0]["inner"] as? [String:String]
+        XCTAssertEqual(3, innerDict1?.count)
+        XCTAssertEqual("newValue", innerDict1?["innerKey"])
+        
+        
+        let innerDict2 = array[1]["inner"] as? [String:String]
+        XCTAssertEqual(2, innerDict2?.count)
+    }
+    
+    func testArrayWildCardInnerMapNotMergeOverwrite(){
+           
+           let toData = """
+           {
+               "array": [
+                   {
+                       "item1": "item1",
+                       "key": "oldValue",
+                       "inner" : {
+                           "innerKey": "oldValue",
+                           "notToMerge": "oldValue"
+
+                       }
+                   },
+                   {
+                       "item2": "item2"
+                   }
+               ]
+           }
+           """.data(using: .utf8)!
+           
+           
+           let fromData = """
+           {
+               "array[*]": {
+                       "key": "newValue",
+                       "inner" : {
+                           "innerKey": "newValue",
+                           "newKey": "newValue",
+                       }
+                   }
+           }
+           """.data(using: .utf8)!
+           
+           let toDict = try! JSONDecoder().decode(AnyCodable.self, from: toData).dictionaryValue!
+           let fromDict = try! JSONDecoder().decode(AnyCodable.self, from: fromData).dictionaryValue!
+           
+           let result = EventDataMerger.merging(to: toDict, from: fromDict, overwrite: false)
+           
+           /*
+             {
+                   "array": [
+                       {
+                           "item1": "item1",
+                           "key": "newValue",
+                           "inner" : {
+                               "innerKey": "oldValue",
+                               "newKey": "newValue",
+                               "notToMerge": "oldValue"
+
+                           }
+                       },
+                       {
+                           "item2": "item2",
+                           "key": "newValue",
+                            "inner" : {
+                                "innerKey": "newValue",
+                                "newKey": "newValue"
+
+                            }
+                       }
+                   ]
+               }
+           */
+           let array =  result["array"] as! [[String:Any]]
+           let innerDict1 = array[0]["inner"] as? [String:String]
+           XCTAssertEqual(3, innerDict1?.count)
+           XCTAssertEqual("oldValue", innerDict1?["innerKey"])
+           
+           
+           let innerDict2 = array[1]["inner"] as? [String:String]
+           XCTAssertEqual(2, innerDict2?.count)
+       }
     
     func testArrayContainsNoneMapItem(){
        
