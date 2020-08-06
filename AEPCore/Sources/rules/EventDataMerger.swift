d566193cf048d4d55c1a9015449f883644049547
jgeng <jgeng@adobe.com>
2020-08-06 10:15:51 -0600

    add some docs

--- AEPCore/Sources/rules/EventDataMerger.swift
+++ AEPCore/Sources/rules/EventDataMerger.swift
@@ -16,21 +16,26 @@ struct EventDataMerger {
     
     private static let SUFFIX_FOR_OBJECT = "[*]"
     
-    /// Merges one dictionary into another. `overwrite` indicates when there is a confict, which value will take priority
+    /// Merges one event data into another. `overwrite` indicates when there is a confict, whether from or to map will take priority
     /// - Parameters:
     ///   - to: the dictionary to be merged to
     ///   - from: the dictionary contains new data
     ///   - overwrite: true if the from dictionary take priority
     static func merging(to: [String: Any?], from: [String: Any?], overwrite: Bool) -> [String:Any] {
         // overwrite all matching key value pairs with new values, and recursively handle nested dictionaries
-        let combinedDictionary = combining(to: to, from: from, overwrite: overwrite)
+        let combinedDictionary = pureMerging(to: to, from: from, overwrite: overwrite)
         // handle the array wild card modification
         let mergedDictionary = handleArrayWildCard(dict: combinedDictionary, overwrite: overwrite)
         // remove nil from the dict
         return mergedDictionary.compactMapValues { $0 }
     }
     
-    static func combining(to: [String: Any?], from: [String: Any?], overwrite: Bool) -> [String:Any?] {
+    /// merge the dictionary, and recursively do the same to the embeded dictionary
+    /// - Parameters:
+    ///   - to: the dictionary to be merged to
+    ///   - from: the dictionary contains new data
+    ///   - overwrite: true if the from dictionary take priority
+    static func pureMerging(to: [String: Any?], from: [String: Any?], overwrite: Bool) -> [String:Any?] {
         // First, overwrite all matching key value pairs with new values, and recursively handle nested dictionaries
         return to.merging(from, uniquingKeysWith: { (old, new) in
             if let newDict = new as? [String: Any?], let oldDict = old as? [String: Any?]  {
@@ -45,11 +50,16 @@ struct EventDataMerger {
         })
     }
     
+    
+    /// deal with the array wild card [*]
+    /// - Parameters:
+    ///   - dict: the dictionary which may have keys end with ` [*]`
+    ///   - overwrite: true if the data in array wild card can overwrite the data in the original array items
     static func handleArrayWildCard(dict: [String: Any?], overwrite:Bool) -> [String: Any?] {
         var dict = dict
         for (k, v) in dict {
             
-            // check for the unique SUFFIX_FOR_OBJECT
+            // check for the unique SUFFIX_FOR_OBJECT ([*])
             guard let range = k.range(of: SUFFIX_FOR_OBJECT) else { continue }
             
             // Remove the special key from the dict
