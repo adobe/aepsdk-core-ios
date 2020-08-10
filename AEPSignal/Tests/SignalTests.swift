41d01db416d21ac741a3d428a31f54d7a1e2ff16
steve benedick <sbenedic@adobe.com>
2020-08-10 11:37:56 -0600

    -signal tests in progress

--- AEPSignal/Tests/SignalTests.swift
+++ AEPSignal/Tests/SignalTests.swift
@@ -34,24 +34,128 @@ class SignalTests: XCTestCase {
         signal = Signal(runtime: mockRuntime)
         signal.onRegistered()
     }
-
+        
+    // MARK: - handleConfigurationResponse(event: Event)
+    /// on privacy opt-out, the hit queue should be cleared
     func testConfigResponseOptOut() throws {
         // setup
-        let data = [SignalConstants.Configuration.GLOBAL_PRIVACY : PrivacyStatus.optedOut.rawValue] as [String : Any]
-        let configEvent = Event(name: "Test Configuration response",
-                                type: EventType.configuration,
-                                source: EventSource.responseContent,
-                                data: data)
-        mockRuntime.simulateSharedState(for: (SignalConstants.Configuration.NAME, configEvent), data: (data, .set))
+        signal.hitQueue.queue(entity: DataEntity(data: nil))
+        XCTAssertEqual(1, signal.hitQueue.count())
         
-        
+        let configEvent = getConfigSharedStateEvent(privacy: .optedOut)
+        mockRuntime.simulateSharedState(for: (SignalConstants.Configuration.NAME, configEvent),
+                                        data: (configEvent.data!, .set))
+                
         // test
         mockRuntime.simulateComingEvents(configEvent)
         
         // verify
-        XCTAssertEqual(0, signal.hitQueue)()
-        
+        XCTAssertEqual(0, signal.hitQueue.count())
     }
 
+    /// on any privacy status other than opt-out, the hit queue should remain unaffected
+    func testConfigResponseOptIn() throws {
+        // setup
+        signal.hitQueue.queue(entity: DataEntity(data: nil))
+        XCTAssertEqual(1, signal.hitQueue.count())
+        
+        let configEvent = getConfigSharedStateEvent(privacy: .optedIn)
+        mockRuntime.simulateSharedState(for: (SignalConstants.Configuration.NAME, configEvent),
+                                        data: (configEvent.data!, .set))
+                
+        // test
+        mockRuntime.simulateComingEvents(configEvent)
+        
+        // verify
+        XCTAssertEqual(1, signal.hitQueue.count())
+    }
 
+    // MARK: - handleRulesEngineResponse(event: Event)
+    /// an event with valid config and postback type should result in a new entry in the hitqueue
+    func testRuleEngineResponsePostbackTypeEvent() throws {
+        // setup
+        let configEvent = getConfigSharedStateEvent(privacy: .optedIn)
+        mockRuntime.simulateSharedState(for: (SignalConstants.Configuration.NAME, configEvent),
+                                        data: (configEvent.data!, .set))
+        let rulesEvent = getRulesResponseEvent(type: "pb")
+                
+        // test
+        mockRuntime.simulateComingEvents(rulesEvent)
+                
+        // verify
+        XCTAssertEqual(1, signal.hitQueue.count())
+    }
+    
+    /// an event with valid config and pii type should result in a new entry in the hitqueue
+    func testRuleEngineResponsePiiTypeEvent() throws {
+        // setup
+        let configEvent = getConfigSharedStateEvent(privacy: .optedIn)
+        mockRuntime.simulateSharedState(for: (SignalConstants.Configuration.NAME, configEvent),
+                                        data: (configEvent.data!, .set))
+        let rulesEvent = getRulesResponseEvent(type: "pii")
+        
+        // test
+        mockRuntime.simulateComingEvents(rulesEvent)
+        
+        // verify
+        
+        XCTAssertEqual(1, signal.hitQueue.count())
+    }
+    
+    /// an event with valid config and open url type should not result in a new entry in the hitqueue
+    
+    /// an event without a valid configuration shared state or an opted-out privacy status should be ignored
+    func testRulesEngineResponseEventShouldBeIgnored() throws {
+        // setup
+        
+        
+        // test
+        
+        
+        // verify
+    }
+    
+    // MARK: - Helpers
+    
+    /// Gets an event to use for simulating Configuration shared state
+    ///
+    /// - Parameter privacy: value to set for privacy status in the returned event
+    /// - Returns: an Event suitable for use with `TestableExtensionRuntime.simulateSharedState`
+    func getConfigSharedStateEvent(privacy: PrivacyStatus) -> Event {
+        let data = [SignalConstants.Configuration.GLOBAL_PRIVACY : privacy.rawValue] as [String : Any]
+        let configEvent = Event(name: "Test Configuration response",
+                                type: EventType.configuration,
+                                source: EventSource.responseContent,
+                                data: data)
+        return configEvent
+    }
+    
+    /// Gets an event to use for simulating Configuration shared state
+    ///
+    /// - Parameter privacy: value to set for privacy status in the returned event
+    /// - Returns: an Event suitable for use with `TestableExtensionRuntime.simulateSharedState`
+    func getRulesResponseEvent(type: String) -> Event {
+        // details are the same for postback and pii, different for open url
+        let details = SignalConstants.EventDataKeys.TYPE == SignalConstants.ConsequenceTypes.OPEN_URL ?
+        [
+            SignalConstants.EventDataKeys.URL : "https://www.testingopenurl.com"
+        ] :
+        [
+            SignalConstants.EventDataKeys.CONTENT_TYPE : "application/json",
+            SignalConstants.EventDataKeys.TEMPLATE_URL : "https://www.postback.com",
+            SignalConstants.EventDataKeys.TEMPLATE_BODY : "{\"key\":\"value\"}",
+            SignalConstants.EventDataKeys.TIMEOUT : 4
+        ]
+        
+        let triggeredConsequence = [SignalConstants.EventDataKeys.TRIGGERED_CONSEQUENCE : [
+            SignalConstants.EventDataKeys.ID : UUID().uuidString,
+            SignalConstants.EventDataKeys.TYPE : type,
+            SignalConstants.EventDataKeys.DETAIL : details
+        ]]
+        let rulesEvent = Event(name: "Test Rules Engine response",
+                                type: EventType.rulesEngine,
+                                source: EventSource.responseContent,
+                                data: triggeredConsequence)
+        return rulesEvent
+    }
 }
