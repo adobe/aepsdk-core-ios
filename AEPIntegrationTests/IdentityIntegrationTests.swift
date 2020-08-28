c8383d85dffbf30053bd39637f15534db3af0887
jgeng <jgeng@adobe.com>
2020-08-27 23:02:03 -0600

    test

--- AEPIntegrationTests/IdentityIntegrationTests.swift
+++ AEPIntegrationTests/IdentityIntegrationTests.swift
@@ -1,35 +1,27 @@
-//
-//  AEPIntegrationTests.swift
-//  AEPIntegrationTests
-//
-//  Created by Jiabin Geng on 8/24/20.
-//  Copyright © 2020 Adobe. All rights reserved.
-//
+/*
+ Copyright 2020 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
 
 import XCTest
-import AEPCore
+@testable import AEPCore
 import AEPServices
 import AEPIdentity
 import AEPLifecycle
 import AEPSignal
 
 
-extension UserDefaults {
-    public static func clear() {
-        for _ in 0 ... 5 {
-            for key in UserDefaults.standard.dictionaryRepresentation().keys {
-                UserDefaults.standard.removeObject(forKey: key)
-            }
-        }
-    }
-}
-
-
-
-class AEPIntegrationTests: XCTestCase {
+class IdentityIntegrationTests: XCTestCase {
 
     override func setUp() {
         UserDefaults.clear()
+        EventHub.reset()
     }
 
     override func tearDown() {
@@ -86,26 +78,56 @@ class AEPIntegrationTests: XCTestCase {
 
         wait(for: [requestExpection], timeout: 1)
     }
-    
-    func testLifecycle() {
+
+    func testIdentityOptout1() {
         initExtensionsAndWait()
 
-        let requestExpection = XCTestExpectation()
-        requestExpection.isInverted = true
-        let mockNetworkService = TestableNetworkService()
-        ServiceProvider.shared.networkService = mockNetworkService
-        mockNetworkService.resolver = { request in
-            if request.url.absoluteString.contains("d_cid_ic=id1%2501value1%25010") {
-                requestExpection.fulfill()
-            }
-            return nil
+        let ecidExpection = XCTestExpectation()
+
+        MobileCore.updateConfigurationWith(configDict: ["experienceCloud.org": "orgid", "experienceCloud.server": "test.com", "global.privacy": "optedin"])
+        Identity.getExperienceCloudId { ecid in
+            XCTAssertTrue(!ecid!.isEmpty)
+            ecidExpection.fulfill()
         }
 
-        MobileCore.updateConfigurationWith(configDict: ["experienceCloud.org": "orgid", "experienceCloud.server": "test.com", "global.privacy": "optedout"])
+        wait(for: [ecidExpection], timeout: 1)
+    }
+
+    func testIdentityOptout2() {
+        initExtensionsAndWait()
+
+        let variablesExpection = XCTestExpectation()
+
+        MobileCore.updateConfigurationWith(configDict: ["experienceCloud.org": "orgid", "experienceCloud.server": "test.com", "global.privacy": "optedin"])
         MobileCore.lifecycleStart(additionalContextData: ["key": "value"])
         Identity.syncIdentifiers(identifiers: ["id1": "value1"])
+        Identity.getUrlVariables { (variables, error) in
 
-        wait(for: [requestExpection], timeout: 1)
+            variablesExpection.fulfill()
+        }
+
+        wait(for: [variablesExpection], timeout: 1)
+    }
+
+    func testIdentityOptout3() {
+        initExtensionsAndWait()
+
+        let urlExpection = XCTestExpectation()
+
+        MobileCore.updateConfigurationWith(configDict: ["experienceCloud.org": "orgid", "experienceCloud.server": "test.com", "global.privacy": "optedin"])
+        MobileCore.lifecycleStart(additionalContextData: ["key": "value"])
+        Identity.syncIdentifiers(identifiers: ["id1": "value1"])
+        let url2 = URL(string: "https://adobe.com")
+        let a = url2?.absoluteString
+
+
+        Identity.appendTo(url: URL(string: "https://adobe.com")) { (url, error) in
+
+            let c = url?.absoluteString
+            urlExpection.fulfill()
+        }
+
+        wait(for: [urlExpection], timeout: 1)
     }
 
 }
