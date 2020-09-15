4261687f7c6ae227d00c68e5a8f9f291def286ec
steve benedick <sbenedic@adobe.com>
2020-09-15 16:56:22 -0600

    #300 - add products for dynamic and static libraries

--- Package.swift
+++ Package.swift
@@ -19,11 +19,26 @@ let package = Package(
     name: "AEPCore",
     platforms: [.iOS(.v10)],
     products: [
+        // default
         .library(name: "AEPCore", targets: ["AEPCore"]),
         .library(name: "AEPIdentity", targets: ["AEPIdentity"]),
         .library(name: "AEPLifecycle", targets: ["AEPLifecycle"]),
         .library(name: "AEPServices", targets: ["AEPServices"]),
         .library(name: "AEPSignal", targets: ["AEPSignal"]),
+        
+        // dynamic
+        .library(name: "AEPCoreDynamic", type: .dynamic, targets: ["AEPCore"]),
+        .library(name: "AEPIdentityDynamic", type: .dynamic, targets: ["AEPIdentity"]),
+        .library(name: "AEPLifecycleDynamic", type: .dynamic, targets: ["AEPLifecycle"]),
+        .library(name: "AEPServicesDynamic", type: .dynamic, targets: ["AEPServices"]),
+        .library(name: "AEPSignalDynamic", type: .dynamic, targets: ["AEPSignal"]),
+        
+        // statics
+        .library(name: "AEPCoreStatic", type: .static, targets: ["AEPCore"]),
+        .library(name: "AEPIdentityStatic", type: .static, targets: ["AEPIdentity"]),
+        .library(name: "AEPLifecycleStatic", type: .static, targets: ["AEPLifecycle"]),
+        .library(name: "AEPServicesStatic", type: .static, targets: ["AEPServices"]),
+        .library(name: "AEPSignalStatic", type: .static, targets: ["AEPSignal"]),
     ],
     dependencies: [
         .package(url: "https://github.com/adobe/aepsdk-rulesengine-ios.git", .branch("dev")),
