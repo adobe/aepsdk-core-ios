1eaa09013a708d6d280a25993ab61c8012f617ae
Christopher Hoffmann <choffman@adobe.com>
2020-09-21 15:30:42 -0600

    test normal startup read app id from manifest

--- AEPCore/Tests/FunctionalTests/Configuration/ConfigurationLifecycleResponseTests.swift
+++ AEPCore/Tests/FunctionalTests/Configuration/ConfigurationLifecycleResponseTests.swift
@@ -1,10 +1,14 @@
-//
-//  ConfigurationLifecycleResponseTests.swift
-//  AEPCoreTests
-//
-//  Created by Christopher Hoffman on 9/17/20.
-//  Copyright © 2020 Adobe. All rights reserved.
-//
+/*
+ Copyright 2020 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
 
 @testable import AEPCore
 import XCTest
@@ -74,5 +78,39 @@ class ConfigurationLifecycleResponseTests: XCTestCase {
 
         mockSystemInfoService.property = nil
     }
+
+    func testHandleValidAppidFromManifest() {
+        let mockSystemInfoService = MockSystemInfoService()
+        mockSystemInfoService.property = "testappid"
+        ServiceProvider.shared.systemInfoService = mockSystemInfoService
+
+        let lifecycleEvent = Event(name: "Lifecycle response content", type: EventType.lifecycle, source: EventSource.responseContent, data: nil)
+
+        // test
+        mockRuntime.resetDispatchedEventAndCreatedSharedStates()
+        mockRuntime.simulateComingEvents(lifecycleEvent)
+
+        // verify
+        XCTAssertEqual(1, mockRuntime.dispatchedEvents.count)
+        XCTAssertEqual(EventType.configuration, mockRuntime.firstEvent?.type)
+        XCTAssertEqual(EventSource.requestContent, mockRuntime.firstEvent?.source)
+        XCTAssertEqual("testappid", mockRuntime.firstEvent?.data?["config.appId"] as? String)
+
+        mockSystemInfoService.property = nil
+
+        // reboot
+        mockRuntime = TestableExtensionRuntime()
+        mockRuntime.resetDispatchedEventAndCreatedSharedStates()
+        configuration = Configuration(runtime: mockRuntime)
+        configuration.onRegistered()
+
+        // verify
+        XCTAssertEqual(1, mockRuntime.dispatchedEvents.count)
+        XCTAssertEqual(EventType.configuration, mockRuntime.firstEvent?.type)
+        XCTAssertEqual(EventSource.requestContent, mockRuntime.firstEvent?.source)
+        XCTAssertEqual("testappid", mockRuntime.firstEvent?.data?["config.appId"] as? String)
+
+    }
+
 
 }
