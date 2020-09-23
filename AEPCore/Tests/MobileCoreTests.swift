8c15f5249b68eadd013b10564032d404a4250559
jgeng <jgeng@adobe.com>
2020-09-23 11:06:48 -0600

    fix a unit test timeout

--- AEPCore/Tests/MobileCoreTests.swift
+++ AEPCore/Tests/MobileCoreTests.swift
@@ -99,7 +99,7 @@ class MobileCoreTests: XCTestCase {
 
 
         // verify
-        wait(for: [expectation], timeout: 0.5)
+        wait(for: [expectation], timeout: 1)
     }
 
     func testRegisterExtensionsSimpleEventDispatch() {
