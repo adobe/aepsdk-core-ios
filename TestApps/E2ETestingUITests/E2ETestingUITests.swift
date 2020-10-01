5541fb609796f5f7ccf733982d4364ae9f540d66
Yansong Yang <yanyang@adobe.com>
2020-10-01 10:40:44 -0500

    add more E2E tests (Lifecycle/Signal) (#349)
    * add more E2E tests
    
    * remove unused  TestApp target from Xcode project
    
    * add E2E (identity) tests

--- TestApps/E2ETestingUITests/E2ETestingUITests.swift
+++ TestApps/E2ETestingUITests/E2ETestingUITests.swift
@@ -14,33 +14,26 @@ import XCTest
 class E2ETestingUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = true
+        let app = XCUIApplication()
+        app.launch()
+        app.buttons["Load AEP SDK"].tap()
+        XCTAssert(app.staticTexts["Eventhub Booted"].waitForExistence(timeout: 5))
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAttachDataRule() throws {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["Verify Attach Data Rule"].tap()
+        XCTAssert(app.staticTexts["Catch the track action event with attached data"].waitForExistence(timeout: 5))
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testModifyDataRule() throws {
+        let app = XCUIApplication()
+        app.buttons["Verify Modify Data Rule"].tap()
+        XCTAssert(app.staticTexts["Catch the track action event with modified data"].waitForExistence(timeout: 5))
     }
 }
