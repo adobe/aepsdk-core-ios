0787bf8ad821d5278da568d752c2357406b2bfcb
steve benedick <sbenedic@adobe.com>
2020-11-02 15:25:33 -0700

    -Z

--- AEPServices/Sources/utility/Date+Format.swift
+++ AEPServices/Sources/utility/Date+Format.swift
@@ -28,7 +28,7 @@ public extension Date {
     func getISO8601DateNoColon() -> String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZ"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter.string(from: self)
     }
 }
