7829cd7733d19e6880a1d591b63db84962c402c0
Nick Porter <nporter@adobe.com>
2020-11-24 10:40:06 -0700

    Don't dispatch app id event when an app id is persisted

--- AEPCore/Sources/configuration/Configuration.swift
+++ AEPCore/Sources/configuration/Configuration.swift
@@ -46,11 +46,6 @@ class Configuration: Extension {
         registerListener(type: EventType.configuration, source: EventSource.requestContent, listener: receiveConfigurationRequest(event:))
         registerListener(type: EventType.lifecycle, source: EventSource.responseContent, listener: receiveLifecycleResponse(event:))
 
-        // If we have an appId stored in persistence, kick off the configureWithAppId event
-        if let appId = appIdManager.loadAppId(), !appId.isEmpty {
-            dispatchConfigurationRequest(data: [ConfigurationConstants.Keys.JSON_APP_ID: appId])
-        }
-
         configState.loadInitialConfig()
         let config = configState.environmentAwareConfiguration
         if !config.isEmpty {
