f25c01efe5ec573e39aeb9b2e9ef64a4d2fffc6a
Nick Porter <nporter@adobe.com>
2021-01-06 14:37:33 -0700

    Add longer test for hex encoding

--- AEPCore/Tests/Data+HexStringTests.swift
+++ AEPCore/Tests/Data+HexStringTests.swift
@@ -26,6 +26,19 @@ class Data_HexStringTests: XCTestCase {
         // verify
         XCTAssertEqual(expected, converted)
     }
+    
+    /// Tests that the string is properly encoded for longer strings
+    func testHexStringFromDataHappyLong() {
+        // setup
+        let data = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.".data(using: .utf8)!
+        let expected = "4c6f72656d20697073756d2c206f72206c697073756d20617320697420697320736f6d6574696d6573206b6e6f776e2c2069732064756d6d792074657874207573656420696e206c6179696e67206f7574207072696e742c2067726170686963206f72207765622064657369676e732e205468652070617373616765206973206174747269627574656420746f20616e20756e6b6e6f776e207479706573657474657220696e2074686520313574682063656e747572792077686f2069732074686f7567687420746f206861766520736372616d626c6564207061727473206f662043696365726f27732044652046696e6962757320426f6e6f72756d206574204d616c6f72756d20666f722075736520696e206120747970652073706563696d656e20626f6f6b2e"
+
+        // test
+        let converted = data.hexDescription
+
+        // verify
+        XCTAssertEqual(expected, converted)
+    }
 
     /// Tests that when an empty string is passed that we return an empty string
     func testHexStringFromDataEmpty() {
