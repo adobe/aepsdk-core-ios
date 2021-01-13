b38d7573b8239c44b6550199b5bfb26623769376
Arjun Bhadra <abhadra@adobe.com>
2021-01-13 08:32:46 -0800

    Implement batchLimit on PersistentHitQueue (#496)
    * Added getCanonicalPlatformName API in SystemInfoService
    
    * [Updated swiftformat] formatting fixes
    
    * Added batchLimit to PersistentHitQueue with unit tests
    
    * Moved batchLimit property to HitProcessing
    
    * Added forceProcessing method to ignoreBatchLimit with unit tests
    
    * Added review fixes
    
    * cleanup
    
    Co-authored-by: Steve Benedick <sbenedic@adobe.com>
    Co-authored-by: Jiabin Geng <gengjiabin@gmail.com>

--- AEPServices/Sources/utility/hitprocessor/HitProcessing.swift
+++ AEPServices/Sources/utility/hitprocessor/HitProcessing.swift
@@ -14,6 +14,9 @@ import Foundation
 /// A class of types who provide the functionality for processing hits
 public protocol HitProcessing: class {
 
+    /// batchLimit: an `Int`  specifies the number of hits to be batched to start processing hits
+    var batchLimit: Int { get set}
+
     /// Determines the interval at which a hit should be retried
     /// - Parameter entity: The hit whose retry interval is to be computed
     func retryInterval(for entity: DataEntity) -> TimeInterval
