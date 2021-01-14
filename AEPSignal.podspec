1fe977a616ffefe1e9aa572641cfb35b5f83bccc
Yansong Yang <yanyang@adobe.com>
2021-01-14 11:24:14 -0600

    Add "release action" to AEPCore repo; bump up version to 3.0.0-beta3 (#500)
    * Add release action to AEPCore repo; bump up version to 3.0.0-beta3
    
    * update all podspec files: always depends on latests (to-be-released) AEPCore/AEPRulesEngine/AEPService

--- AEPSignal.podspec
+++ AEPSignal.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "AEPSignal"
-  s.version          = "3.0.0-beta.2"
+  s.version          = "3.0.0-beta.3"
   s.summary          = "Signal extension for Adobe Experience Platform Mobile SDK. Written and maintained by Adobe."
   s.description      = <<-DESC
                         The AEPSignal extension provides the support for Postback/PII/Open URL actions triggered by Core rules engine.
@@ -17,6 +17,6 @@ Pod::Spec.new do |s|
   s.swift_version = '5.1'
   s.pod_target_xcconfig = { 'BUILD_LIBRARY_FOR_DISTRIBUTION' => 'YES' }
 
-  s.dependency 'AEPCore'
+  s.dependency 'AEPCore', '3.0.0-beta.3'
 
 end
---
+++ Script/test-podspec.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+PROJECT_NAME=TestProject
+
+# Clean up.
+rm -rf $PROJECT_NAME
+
+mkdir -p $PROJECT_NAME && cd $PROJECT_NAME
+
+# Create a new Xcode project.
+swift package init
+swift package generate-xcodeproj
+
+# Create a Podfile with our pod as dependency.
+echo "
+platform :ios, '10.0'
+target '$PROJECT_NAME' do
+  use_frameworks!
+  pod 'AEPCore', :path => '../AEPCore.podspec'
+  pod 'AEPIdentity', :path => '../AEPIdentity.podspec'
+  pod 'AEPLifecycle', :path => '../AEPLifecycle.podspec'
+  pod 'AEPServices', :path => '../AEPServices.podspec'
+  pod 'AEPSignal', :path => '../AEPSignal.podspec'
+  pod 'AEPRulesEngine', '1.0.0-beta.2'
+end
+" >>Podfile
+
+# Install the pods.
+pod install
+
+# Archive for generic iOS device
+echo '############# Archive for generic iOS device ###############'
+xcodebuild archive -scheme TestProject-Package -workspace TestProject.xcworkspace -destination 'generic/platform=iOS'
+
+# Build for generic iOS device
+echo '############# Build for generic iOS device ###############'
+xcodebuild clean build -scheme TestProject-Package -workspace TestProject.xcworkspace -destination 'generic/platform=iOS'
+
+# Archive for x86_64 simulator
+echo '############# Archive for simulator ###############'
+xcodebuild archive -scheme TestProject-Package -workspace TestProject.xcworkspace -destination 'generic/platform=iOS Simulator'
+
+# Build for x86_64 simulator
+echo '############# Build for simulator ###############'
+xcodebuild clean build -scheme TestProject-Package -workspace TestProject.xcworkspace -destination 'generic/platform=iOS Simulator'
+
+# Clean up.
+cd ../
+rm -rf $PROJECT_NAME
