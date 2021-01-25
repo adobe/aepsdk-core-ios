b6c381deed0141f6f4a1f6e10599bf7962c39779
ravjain-adb <61328814+ravjain-adb@users.noreply.github.com>
2021-01-25 13:27:58 -0800

    Improvements to UI service with full screen message (#493)
    * Removing the handler
    
    * Simplyfying UI service
    
    * Changes based on comments
    
    * Adding global listener for any message
    
    * Updating the global listeners
    
    * Adding Current message in UIService
    
    * Adding currentMessage in protocol
    
    * Updating comments
    
    * Updating tests
    
    * changes
    
    * Updating message monitor
    
    * Simplying the ui services fullscreen message
    
    * Updating the test
    
    * Updating comments
    
    * Updates with alert
    
    * Updating lint errors
    
    * Updating show method of fullscreen message
    
    * Updating some tests
    
    * Updating based on review and comments tests
    
    * Updating listeners with message object and updating tests
    
    * Fixing the Lint errors
    
    * Changes for service
    
    * Adding protocol to msg monitor service
    
    * Adding a protocol UIMessaging for messages
    
    * Updates to tests
    
    * Fixing lint
    
    * Updating tests
    
    * Renaming and updating comments
    
    * Remove unecessary code

--- AEPServices/Sources/ServiceProvider.swift
+++ AEPServices/Sources/ServiceProvider.swift
@@ -33,8 +33,7 @@ public class ServiceProvider {
     private var overrideURLService: URLOpening?
     private var defaultURLService = URLService()
     private var defaultLoggingService = LoggingService()
-    private var overrideFullscreenUIService: UIServiceInterface?
-    private var defaultFullscreenUIService = UIService()
+    private var defaultMessageMonitorService = MessageMonitorService()
 
     // Don't allow init of ServiceProvider outside the class
     private init() {}
@@ -117,15 +116,10 @@ public class ServiceProvider {
         }
     }
 
-    public var uiService: UIServiceInterface {
+    var messageMonitorService: MessageMonitorServicing {
         get {
             return queue.sync {
-                return overrideFullscreenUIService ?? defaultFullscreenUIService
-            }
-        }
-        set {
-            queue.async {
-                self.overrideFullscreenUIService = newValue
+                return defaultMessageMonitorService
             }
         }
     }
@@ -146,13 +140,12 @@ public class ServiceProvider {
         defaultCacheService = DiskCacheService()
         defaultURLService = URLService()
         defaultLoggingService = LoggingService()
-        defaultFullscreenUIService = UIService()
+        defaultMessageMonitorService = MessageMonitorService()
 
         overrideSystemInfoService = nil
         overrideKeyValueService = nil
         overrideNetworkService = nil
         overrideCacheService = nil
         overrideURLService = nil
-        overrideFullscreenUIService = nil
     }
 }
