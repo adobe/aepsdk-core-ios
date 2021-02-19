1cab9c40c4b1dd0083283e76ee4b384b13a53e94
Kevin Lind <lind@adobe.com>
2021-02-18 18:04:16 -0800

    Revert "Handle AdId to zero or empty on first launch (#530)"
    This reverts commit 670e707ae71deec39c913454de411ebd7c533fcc.

--- AEPIdentity/Sources/IdentityState.swift
+++ AEPIdentity/Sources/IdentityState.swift
@@ -269,11 +269,7 @@ class IdentityState {
     /// - Returns: A tuple indicating if the ad id has changed, and if the consent flag should be added
     private func shouldUpdateAdId(newAdID: String?) -> (adIdChanged: Bool, addConsentFlag: Bool) {
         guard let newAdID = newAdID else { return (false, false) }
-        guard let existingAdId = identityProperties.advertisingIdentifier else {
-            // existing is nil but new is not, update with new and update consent
-            // covers first call case where existing ad ID is not set and new ad ID is empty/all zeros
-            return (true, true)
-        }
+        let existingAdId = identityProperties.advertisingIdentifier ?? ""
 
         // did the advertising identifier change?
         if (!newAdID.isEmpty && newAdID != existingAdId)
