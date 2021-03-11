637157ebd3fdc71ef6b43f8cde8e1a0c3af0fe89
Yansong <yanyang@adobe.com>
2021-03-10 18:46:25 -0600

    fix rename issue

--- AEPLifecycle/Sources/Lifecycle.swift
+++ AEPLifecycle/Sources/Lifecycle.swift
@@ -132,7 +132,7 @@ public class Lifecycle: NSObject, Extension {
                                type: EventType.lifecycle,
                                source: EventSource.responseContent,
                                data: eventData)
-        Log.trace(label: LifecycleConstants.LOG_TAG, "Dispatching lifecycle start event with data: \n\(EventDataPrinter.prettify(eventData))")
+        Log.trace(label: LifecycleConstants.LOG_TAG, "Dispatching lifecycle start event with data: \n\(PrettyDictionary.prettify(eventData))")
         dispatch(event: startEvent)
     }
 
