a7e7ac61f25f2613d91b6188a0cd00e3e1170dd8
Nick Porter <nporter@adobe.com>
2021-04-06 10:51:42 -0600

    Re-write test

--- AEPCore/Tests/MobileCoreTests.swift
+++ AEPCore/Tests/MobileCoreTests.swift
@@ -223,16 +223,20 @@ class MobileCoreTests: XCTestCase {
 
         // test
         MobileCore.registerExtensions([MockExtension.self, MockExtensionTwo.self], {
-            sleep(UInt32(2))
-            let registered = MobileCore.getRegisteredExtensions()
-            let registeredDict = self.jsonStrToDict(jsonStr: registered)?["extensions"] as? Dictionary<String, Any>
-            let equal = NSDictionary(dictionary: registeredDict!).isEqual(to: expectedDict!)
-            XCTAssertTrue(equal)
-            expectation.fulfill()
+            EventHub.shared.getExtensionContainer(MockExtension.self)?.registerListener(type: EventType.hub, source: EventSource.sharedState) { event in
+                if event.data?[EventHubConstants.EventDataKeys.Configuration.EVENT_STATE_OWNER] as? String == EventHubConstants.NAME {
+                    let registered = MobileCore.getRegisteredExtensions()
+                    let registeredDict = self.jsonStrToDict(jsonStr: registered)?["extensions"] as? Dictionary<String, Any>
+                    let equal = NSDictionary(dictionary: registeredDict!).isEqual(to: expectedDict!)
+                    XCTAssertTrue(equal)
+                    expectation.fulfill()
+                    
+                }
+            }
         })
 
         // verify
-        wait(for: [expectation], timeout: 0.25)
+        wait(for: [expectation], timeout: 1)
     }
 
     private func jsonStrToDict(jsonStr: String) -> [String: Any]? {
