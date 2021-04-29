028a8b4da47481794514d16dab9305055ba9a617
Jiabin Geng <gengjiabin@gmail.com>
2021-04-29 10:02:05 -0600

    use version-based dependency to include rules engine in package.swift (#615)
    Co-authored-by: jgeng <jgeng@adobe.com>

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .library(name: "AEPSignal", targets: ["AEPSignal"])
     ],
     dependencies: [
-        .package(url: "https://github.com/adobe/aepsdk-rulesengine-ios.git", .branch("main")),
+        .package(url: "https://github.com/adobe/aepsdk-rulesengine-ios.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "AEPCore",
