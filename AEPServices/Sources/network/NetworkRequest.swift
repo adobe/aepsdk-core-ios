5b05e108e6650217698c966b40e36981abfbe2ca
pravin <pravinpk0707@gmail.com>
2021-05-20 11:23:37 -0700

    [AMSDK-14325] - Use convenience constructor + test for network service connectpayloadWithData

--- AEPServices/Sources/network/NetworkRequest.swift
+++ AEPServices/Sources/network/NetworkRequest.swift
@@ -33,30 +33,24 @@ import Foundation
     ///   - connectTimeout: optional connect timeout value in seconds; default is 5 seconds
     ///   - readTimeout: optional read timeout value in seconds, used to wait for a read to finish after a successful connect, default is 5 seconds
     /// - Returns: an initialized `NetworkRequest` object
-    public init(url: URL, httpMethod: HttpMethod = HttpMethod.get, connectPayload: String = "", httpHeaders: [String: String] = [:], connectTimeout: TimeInterval = 5, readTimeout: TimeInterval = 5) {
-        self.url = url
-        self.httpMethod = httpMethod
-        self.connectPayload = connectPayload.data(using: .utf8) ?? Data()
-
-        let systemInfoService = ServiceProvider.shared.systemInfoService
-        let defaultHeaders = [NetworkRequest.REQUEST_HEADER_KEY_USER_AGENT: systemInfoService.getDefaultUserAgent(),
-                              HttpConnectionConstants.Header.HTTP_HEADER_KEY_ACCEPT_LANGUAGE: DefaultHeadersFormatter.formatLocale(systemInfoService.getActiveLocaleName())]
-        self.httpHeaders = defaultHeaders.merging(httpHeaders) { _, new in new } // add in default headers and apply `httpHeaders` on top
-
-        self.connectTimeout = connectTimeout
-        self.readTimeout = readTimeout
+    public convenience init(url: URL, httpMethod: HttpMethod = HttpMethod.get, connectPayload: String = "", httpHeaders: [String: String] = [:], connectTimeout: TimeInterval = 5, readTimeout: TimeInterval = 5) {
+        self.init(url: url,
+                  httpMethod: httpMethod,
+                  connectPayloadData: connectPayload.data(using: .utf8) ?? Data(),
+                  httpHeaders: httpHeaders,
+                  connectTimeout: connectTimeout,
+                  readTimeout: readTimeout)
     }
 
-    /// Initialize the `NetworkRequest` to post data.
-    /// The instance created from this initializer default to making http post network call.
+    /// Initialize the `NetworkRequest`
     /// - Parameters:
     ///   - url: URL used to initiate the network connection, should use https scheme
-    ///   - connectPayloadData: the body of the network request as a String; this parameter is ignored for GET requests
+    ///   - connectPayloadData: the body of the network request as a Data
     ///   - httpHeaders: optional HTTP headers for the request
     ///   - connectTimeout: optional connect timeout value in seconds; default is 5 seconds
     ///   - readTimeout: optional read timeout value in seconds, used to wait for a read to finish after a successful connect, default is 5 seconds
     /// - Returns: an initialized `NetworkRequest` object
-    public init(url: URL, connectPayloadData: Data = Data(), httpHeaders: [String: String] = [:], connectTimeout: TimeInterval = 5, readTimeout: TimeInterval = 5) {
+    public init(url: URL, httpMethod: HttpMethod = HttpMethod.get, connectPayloadData: Data, httpHeaders: [String: String] = [:], connectTimeout: TimeInterval = 5, readTimeout: TimeInterval = 5) {
         self.url = url
         self.httpMethod = .post
         self.connectPayload = connectPayloadData
