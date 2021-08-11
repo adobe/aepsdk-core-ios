95d04d742242a998a8b955979e17f8660fc5b404
steve benedick <sbenedic@adobe.com>
2021-08-11 17:08:58 -0600

    -background tap to dismiss

--- AEPServices/Sources/ui/fullscreen/FullscreenMessage.swift
+++ AEPServices/Sources/ui/fullscreen/FullscreenMessage.swift
@@ -232,7 +232,6 @@ public class FullscreenMessage: NSObject, FullscreenPresentable {
         // if this is a ui takeover, add an invisible view over under the webview
         if let takeover = settings?.uiTakeover, takeover {
             transparentBackgroundView = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight))
-            transparentBackgroundView?.alpha = 0
             let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
             transparentBackgroundView?.addGestureRecognizer(tap)
         }
@@ -268,7 +267,7 @@ public class FullscreenMessage: NSObject, FullscreenPresentable {
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
-        dismissWithAnimation(shouldDeallocateWebView: true)
+        dismiss()
     }
     
     private func displayWithAnimation(webView: WKWebView) {
