7715645e466aa4d9ba452eecf94a484f9cae4bfb
Emilia Dobrin <33132425+emdobrin@users.noreply.github.com>
2021-08-18 22:54:21 -0700

    Lifecycle - Codebase updates to better accommodate standard & XDM  workflows (#694)
    * [MOB-14879] Move relavant classes to LifecycleV2 group, renaming
    
    * [MOB-14879] Separate constants for v2
    
    * [MOB-14879] Make copyright format same in all files
    
    * [MOB-14879] Removed previous session info from application.close event & shared isInstall between standard/xdm
    
    * [MOB-14879] Cleanup - remove unused sessionLength from LifecycleSession
    
    * [MOB-14879] Refactor LifecycleV2MetricsBuilder, fix app version computation
    
    * [MOB-14879] Cleanup - sort test files
    
    * [MOB-14879] Re-arrange files and fix isUpgrade
    
    * [MOB-14879] Don't set upgrade if is install
    
    * [MOB-14879] Cleanup, formatting
    
    * [MOB-14879] Update installDate in first launch
    
    * swiftformat
    
    * [MOB-14879] Cleanup, renaming

--- AEPLifecycle/Sources/LifecycleState.swift
+++ AEPLifecycle/Sources/LifecycleState.swift
@@ -3,6 +3,7 @@
  This file is licensed to you under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License. You may obtain a copy
  of the License at http://www.apache.org/licenses/LICENSE-2.0
+
  Unless required by applicable law or agreed to in writing, software distributed under
  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  OF ANY KIND, either express or implied. See the License for the specific language
@@ -55,19 +56,26 @@ struct LifecycleState {
     ///   - additionalContextData: additional context data for this start event
     ///   - adId: The advertising identifier provided by the identity extension
     ///   - sessionTimeout: The session timeout for this start event, defaults to 300 seconds
+    ///   - isInstall: Indicates whether this is an application install scenario
     /// - Returns: The previous session info if exists, otherwise nil
-    mutating func start(date: Date, additionalContextData: [String: Any]?, adId: String?, sessionTimeout: TimeInterval = TimeInterval(LifecycleConstants.DEFAULT_LIFECYCLE_TIMEOUT)) -> LifecycleSessionInfo? {
+    mutating func start(date: Date,
+                        additionalContextData: [String: Any]?,
+                        adId: String?,
+                        sessionTimeout: TimeInterval = TimeInterval(LifecycleConstants.DEFAULT_LIFECYCLE_TIMEOUT),
+                        isInstall: Bool) -> LifecycleSessionInfo? {
         let sessionContainer: LifecyclePersistedContext? = dataStore.getObject(key: LifecycleConstants.DataStoreKeys.PERSISTED_CONTEXT)
         // Build default LifecycleMetrics
         let metricsBuilder = LifecycleMetricsBuilder(dataStore: dataStore, date: date).addDeviceData()
         let defaultMetrics = metricsBuilder.build()
-        applyApplicationUpgrade(appId: defaultMetrics.appId)
+        if !isInstall {
+            applyApplicationUpgrade(appId: defaultMetrics.appId)
+        }
 
         guard let previousSessionInfo = lifecycleSession.start(date: date, sessionTimeout: sessionTimeout, coreMetrics: defaultMetrics) else { return nil }
 
         var lifecycleData = LifecycleContextData()
 
-        if isInstall() {
+        if isInstall {
             metricsBuilder.addInstallData().addLaunchEventData()
         } else {
             // upgrade and launch hits
@@ -89,12 +97,6 @@ struct LifecycleState {
         // Update lifecycle context data and persist lifecycle info into local storage
         lifecycleContextData = lifecycleData
 
-        let xdmMetricsBuilder = XDMLifecycleMetricsBuilder(startDate: date)
-            .addAppLaunchData(isInstall: isInstall(), isUpgrade: isUpgrade())
-            .addAppCloseData(previousAppId: sessionContainer?.appId, previousSessionInfo: previousSessionInfo)
-            .addEnvironmentData()
-            .addDeviceData()
-
         persistLifecycleContextData(startDate: date)
 
         return previousSessionInfo
@@ -114,8 +116,8 @@ struct LifecycleState {
     /// Updates the application identifier in the in-memory lifecycle context data
     /// - Parameter appId: the application identifier
     mutating func applyApplicationUpgrade(appId: String?) {
-        // early out if this isn't an upgrade or if it is an install
-        if isInstall() || !isUpgrade() { return }
+        // early out if this isn't an upgrade
+        if !isUpgrade() { return }
 
         // get a map of lifecycle data in shared preferences or memory
         guard var lifecycleData = getContextData() else { return }
@@ -134,11 +136,6 @@ struct LifecycleState {
     }
 
     // MARK: Private APIs
-
-    /// Returns true if there is not install date stored in the data store
-    private func isInstall() -> Bool {
-        return !dataStore.contains(key: LifecycleConstants.DataStoreKeys.INSTALL_DATE)
-    }
 
     /// Returns true if the current app version does not equal the app version stored in the data store
     private func isUpgrade() -> Bool {
---
+++ AEPLifecycle/Sources/LifecycleV2/LifecycleV2.swift
@@ -0,0 +1,70 @@
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import AEPCore
+import AEPServices
+import Foundation
+
+/// The responsibility of `LifecycleV2` is to compute the application launch/close XDM metrics,
+/// usually consumed by the Edge Network and related extensions
+class LifecycleV2 {
+    private let dataStore: NamedCollectionDataStore
+    private var systemInfoService: SystemInfoService {
+        ServiceProvider.shared.systemInfoService
+    }
+
+    /// Creates a new `LifecycleV2` with the given `NamedCollectionDataStore`
+    ///
+    /// - Parameter dataStore: The `NamedCollectionDataStore` used for reading and writing data to persistence
+    init(dataStore: NamedCollectionDataStore) {
+        self.dataStore = dataStore
+    }
+
+    /// Handles the start use-case as application launch XDM event. If a previous abnormal close was detected,
+    /// an application close event is dispatched first.
+    ///
+    /// - Parameters:
+    ///   - date: date at which the start event occurred
+    ///   - additionalData: additional data received with the start event
+    ///   - isInstall: indicates whether this is an application install scenario
+    func start(date: Date,
+               additionalData: [String: Any]?,
+               isInstall: Bool) {
+        // todo: MOB-14878 create launch application event and if needed the close event
+        persistAppVersion()
+
+    }
+
+    /// Handles the pause use-case as application close XDM event.
+    ///
+    /// - Parameter pauseDate: Date at which the pause event occurred
+    func pause(pauseDate: Date) {
+        // todo: MOB-14878 create close application event
+
+    }
+
+    /// - Returns: Bool indicating whether the app has been upgraded
+    private func isUpgrade() -> Bool {
+        if let currentAppVersion = systemInfoService.getApplicationVersion(),
+           let previousAppVersion = dataStore.getString(key: LifecycleV2Constants.DataStoreKeys.LAST_APP_VERSION) {
+            return previousAppVersion != currentAppVersion
+        }
+
+        return false
+    }
+
+    /// Persist the application version into dataStore
+    private func persistAppVersion() {
+        guard let currentAppVersion = systemInfoService.getApplicationVersion() else { return }
+        dataStore.set(key: LifecycleV2Constants.DataStoreKeys.LAST_APP_VERSION, value: currentAppVersion)
+    }
+}
