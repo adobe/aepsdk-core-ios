ff1bf4b722d2676d4925b3e05ab37264456667b4
Christopher Hoffmann <choffman@adobe.com>
2021-08-19 17:59:53 -0400

    swift format

--- TestApps/TestApp_Swift/AppDelegate.swift
+++ TestApps/TestApp_Swift/AppDelegate.swift
@@ -1,13 +1,13 @@
 /*
-Copyright 2021 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
 
 import UIKit
 import AEPCore
@@ -21,7 +21,7 @@ import AEPAssurance
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     private let LAUNCH_ENVIRONMENT_FILE_ID = ""
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let appState = application.applicationState
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                           Lifecycle.self,
                           Signal.self,
                           Assurance.self]
-        
+
         MobileCore.registerExtensions(extensions, {
             MobileCore.configureWith(appId: self.LAUNCH_ENVIRONMENT_FILE_ID)
             if appState != .background {
--- TestApps/TestApp_Swift/AssuranceView.swift
+++ TestApps/TestApp_Swift/AssuranceView.swift
@@ -1,13 +1,13 @@
 /*
-Copyright 2021 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
 
 import SwiftUI
 import AEPCore
@@ -15,7 +15,7 @@ import AEPAssurance
 
 struct AssuranceView: View {
     @State private var assuranceSessionUrl:String = ""
-    
+
     var body: some View {
         VStack(alignment: HorizontalAlignment.leading, spacing: 12) {
             TextField("Copy Assurance Session URL to here", text: $assuranceSessionUrl)
