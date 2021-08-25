1f33a73bc082c7a30c4693673ada643905d3ded1
Christopher Hoffmann <choffman@adobe.com>
2021-08-25 16:27:18 -0600

    Added embedded frameworks to objc app and removed config id

--- TestApps/TestApp_Objc/AppDelegate.m
+++ TestApps/TestApp_Objc/AppDelegate.m
@@ -30,7 +30,7 @@ governing permissions and limitations under the License.
      
     [AEPMobileCore registerExtensions:@[AEPMobileLifecycle.class, AEPMobileSignal.class, AEPMobileIdentity.class] completion:^{
         [AEPMobileCore updateConfiguration:@{@"lifecycle.sessionTimeout":@1}];
-        [AEPMobileCore configureWithAppId:@"94f571f308d5/07c3fc1109d1/launch-2f01e36464da-development"];
+        [AEPMobileCore configureWithAppId:@""];
         
     }];
     
