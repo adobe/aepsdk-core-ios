16a5a55decbb8ce5c33b5135bbc6f24c6bdd2104
Christopher Hoffmann <chrisdhoff28@gmail.com>
2022-04-19 16:53:44 -0600

    add test app extension

---
+++ TestApps/TestAppExtension/FileProviderExtension.swift
@@ -0,0 +1,177 @@
+/*
+ Copyright 2022 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import FileProvider
+import AEPCore
+import AEPServices
+import AEPLifecycle
+import AEPIdentity
+
+class FileProviderExtension: NSFileProviderExtension {
+    
+    var fileManager = FileManager()
+    private let LAUNCH_ENVIRONMENT_FILE_ID = ""
+    
+    override init() {
+        super.init()
+        MobileCore.setLogLevel(.trace)
+        let extensions = [Identity.self,
+                          Lifecycle.self]
+
+        MobileCore.registerExtensions(extensions, {
+            MobileCore.configureWith(appId: self.LAUNCH_ENVIRONMENT_FILE_ID)
+            MobileCore.lifecycleStart(additionalContextData: nil)
+        })
+
+    }
+    
+    override func item(for identifier: NSFileProviderItemIdentifier) throws -> NSFileProviderItem {
+        // resolve the given identifier to a record in the model
+        
+        // TODO: implement the actual lookup
+        return FileProviderItem()
+    }
+    
+    override func urlForItem(withPersistentIdentifier identifier: NSFileProviderItemIdentifier) -> URL? {
+        // resolve the given identifier to a file on disk
+        guard let item = try? item(for: identifier) else {
+            return nil
+        }
+        
+        // in this implementation, all paths are structured as <base storage directory>/<item identifier>/<item file name>
+        let manager = NSFileProviderManager.default
+        let perItemDirectory = manager.documentStorageURL.appendingPathComponent(identifier.rawValue, isDirectory: true)
+        
+        return perItemDirectory.appendingPathComponent(item.filename, isDirectory:false)
+    }
+    
+    override func persistentIdentifierForItem(at url: URL) -> NSFileProviderItemIdentifier? {
+        // resolve the given URL to a persistent identifier using a database
+        let pathComponents = url.pathComponents
+        
+        // exploit the fact that the path structure has been defined as
+        // <base storage directory>/<item identifier>/<item file name> above
+        assert(pathComponents.count > 2)
+        
+        return NSFileProviderItemIdentifier(pathComponents[pathComponents.count - 2])
+    }
+    
+    override func providePlaceholder(at url: URL, completionHandler: @escaping (Error?) -> Void) {
+        guard let identifier = persistentIdentifierForItem(at: url) else {
+            completionHandler(NSFileProviderError(.noSuchItem))
+            return
+        }
+
+        do {
+            let fileProviderItem = try item(for: identifier)
+            let placeholderURL = NSFileProviderManager.placeholderURL(for: url)
+            try NSFileProviderManager.writePlaceholder(at: placeholderURL,withMetadata: fileProviderItem)
+            completionHandler(nil)
+        } catch let error {
+            completionHandler(error)
+        }
+    }
+
+    override func startProvidingItem(at url: URL, completionHandler: @escaping ((_ error: Error?) -> Void)) {
+        // Should ensure that the actual file is in the position returned by URLForItemWithIdentifier:, then call the completion handler
+        
+        /* TODO:
+         This is one of the main entry points of the file provider. We need to check whether the file already exists on disk,
+         whether we know of a more recent version of the file, and implement a policy for these cases. Pseudocode:
+         
+         if !fileOnDisk {
+             downloadRemoteFile()
+             callCompletion(downloadErrorOrNil)
+         } else if fileIsCurrent {
+             callCompletion(nil)
+         } else {
+             if localFileHasChanges {
+                 // in this case, a version of the file is on disk, but we know of a more recent version
+                 // we need to implement a strategy to resolve this conflict
+                 moveLocalFileAside()
+                 scheduleUploadOfLocalFile()
+                 downloadRemoteFile()
+                 callCompletion(downloadErrorOrNil)
+             } else {
+                 downloadRemoteFile()
+                 callCompletion(downloadErrorOrNil)
+             }
+         }
+         */
+        
+        completionHandler(NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo:[:]))
+    }
+    
+    
+    override func itemChanged(at url: URL) {
+        // Called at some point after the file has changed; the provider may then trigger an upload
+        
+        /* TODO:
+         - mark file at <url> as needing an update in the model
+         - if there are existing NSURLSessionTasks uploading this file, cancel them
+         - create a fresh background NSURLSessionTask and schedule it to upload the current modifications
+         - register the NSURLSessionTask with NSFileProviderManager to provide progress updates
+         */
+    }
+    
+    override func stopProvidingItem(at url: URL) {
+        // Called after the last claim to the file has been released. At this point, it is safe for the file provider to remove the content file.
+        // Care should be taken that the corresponding placeholder file stays behind after the content file has been deleted.
+        
+        // Called after the last claim to the file has been released. At this point, it is safe for the file provider to remove the content file.
+        
+        // TODO: look up whether the file has local changes
+        let fileHasLocalChanges = false
+        
+        if !fileHasLocalChanges {
+            // remove the existing file to free up space
+            do {
+                _ = try FileManager.default.removeItem(at: url)
+            } catch {
+                // Handle error
+            }
+            
+            // write out a placeholder to facilitate future property lookups
+            self.providePlaceholder(at: url, completionHandler: { error in
+                // TODO: handle any error, do any necessary cleanup
+            })
+        }
+    }
+    
+    // MARK: - Actions
+    
+    /* TODO: implement the actions for items here
+     each of the actions follows the same pattern:
+     - make a note of the change in the local model
+     - schedule a server request as a background task to inform the server of the change
+     - call the completion block with the modified item in its post-modification state
+     */
+    
+    // MARK: - Enumeration
+    
+    override func enumerator(for containerItemIdentifier: NSFileProviderItemIdentifier) throws -> NSFileProviderEnumerator {
+        let maybeEnumerator: NSFileProviderEnumerator? = nil
+        if (containerItemIdentifier == NSFileProviderItemIdentifier.rootContainer) {
+            // TODO: instantiate an enumerator for the container root
+        } else if (containerItemIdentifier == NSFileProviderItemIdentifier.workingSet) {
+            // TODO: instantiate an enumerator for the working set
+        } else {
+            // TODO: determine if the item is a directory or a file
+            // - for a directory, instantiate an enumerator of its subitems
+            // - for a file, instantiate an enumerator that observes changes to the file
+        }
+        guard let enumerator = maybeEnumerator else {
+            throw NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo:[:])
+        }
+        return enumerator
+    }
+    
+}
