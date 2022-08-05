56a389bb58a82ff6cf1bcb39e1281d85ddd28e2f
Christopher Hoffmann <chrisdhoff28@gmail.com>
2022-08-05 13:49:55 -0600

    Finish adding buttons

--- TestApps/TestApp_Swift/CoreView.swift
+++ TestApps/TestApp_Swift/CoreView.swift
@@ -9,11 +9,125 @@
  governing permissions and limitations under the License.
  */
 
+import UIKit
 import SwiftUI
 
+import AEPCore
+
 struct CoreView: View {
+    @State private var currentPrivacyStatus: String = ""
+    @State private var showingAlert = false
+    @State private var retrievedAttributes: String = ""
+    @State private var eventName: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 12) {
+                privacySection
+                piiSection
+                manualOverridesSection
+                eventsSection
+            }.padding()
+        }
+    }
+
+    var privacySection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Change Privacy Status").bold()
+            Button(action: {
+                // step-privacy-start
+                MobileCore.setPrivacyStatus(.optedIn)
+                // step-privacy-end
+            }) {
+                Text("Opted In")
+            }.buttonStyle(CustomButtonStyle())
+
+            Button(action: {
+                // step-privacy-start
+                MobileCore.setPrivacyStatus(.optedOut)
+                // step-privacy-end
+            }) {
+                Text("Opted Out")
+            }.buttonStyle(CustomButtonStyle())
+
+            Button(action: {
+                // step-privacy-start
+                MobileCore.setPrivacyStatus(.unknown)
+                // step-privacy-end
+            }) {
+                Text("Unknown")
+            }.buttonStyle(CustomButtonStyle())
+
+            HStack {
+                Button(action: {
+                    // step-privacy-start
+                    MobileCore.getPrivacyStatus { privacyStatus in
+                        self.currentPrivacyStatus = "\(privacyStatus.rawValue)"
+                    }
+                    // step-privacy-end
+                }) {
+                    Text("Get Privacy")
+                }.buttonStyle(CustomButtonStyle())
+                VStack{
+                    Text("Current Privacy:")
+                    Text(currentPrivacyStatus)
+                }
+            }
+        }
+    }
+
+    var piiSection: some View {
+
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Collect PII").bold()
+            Button(action: {
+                // step-pii-start
+                MobileCore.collectPii(["name":"Adobe Experience Platform"])
+                // step-pii-end
+            }){
+                Text("Collect PII")
+            }.buttonStyle(CustomButtonStyle())
+        }
+    }
+
+
+    var manualOverridesSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Update Configuration").bold()
+            Button(action: {
+                // step-config-start
+                let dataDict = ["analytics.batchLimit": 3]
+                MobileCore.updateConfigurationWith(configDict: dataDict)
+                // step-config-end
+            }) {
+                Text("Update Configuration")
+            }.buttonStyle(CustomButtonStyle())
+        }
+    }
+
+    var eventsSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Dispatch Events").bold()
+            Button(action: {
+                let event = Event(name: "Sample Event", type: "type", source: "source", data: ["platform" : "ios"])
+                MobileCore.dispatch(event: event)
+            }) {
+                Text("Dispatch Custom Event")
+            }.buttonStyle(CustomButtonStyle())
+
+            Button(action: {
+                let event = Event(name: "Sample Event", type: "type", source: "source", data: ["platform" : "ios"])
+                MobileCore.dispatch(event: event) { event in
+                    self.eventName = event?.name ?? ""
+                }
+            }) {
+                Text("Dispatch Custom Event with response callback")
+            }.buttonStyle(CustomButtonStyle())
+            VStack {
+                Text("Custom Event Response:")
+                Text(eventName)
+            }
+        }
     }
 }
 
--- TestApps/TestApp_Swift/IdentityView.swift
+++ TestApps/TestApp_Swift/IdentityView.swift
@@ -10,10 +10,92 @@
  */
 
 import SwiftUI
+import AEPCore
+import AEPIdentity
 
 struct IdentityView: View {
+
+    @State private var experienceCloudId: String = ""
+    @State private var trackingIdentifier: String = ""
+    @State private var visitorIdentifier: String = ""
+    @State private var urlVariables: String = ""
+    @State private var sdkIdentities: String = ""
+    @State private var url: String = "https://example.com"
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Identity APIs").bold()
+            Button(action: {
+                MobileCore.setAdvertisingIdentifier("advertisingIdentifier")
+            }) {
+                Text("Set Advertising Identifier")
+            }.buttonStyle(CustomButtonStyle())
+
+            Button(action: {
+                MobileCore.setPushIdentifier("9516258b6230afdd93cf0cd07b8dd845".data(using: .utf8))
+            }) {
+                Text("Set Push Identifier")
+            }.buttonStyle(CustomButtonStyle())
+
+            Button(action: {
+                Identity.syncIdentifiers(identifiers: ["idType1":"1234567"], authenticationState: .authenticated)
+            }) {
+                Text("Sync Identifiers")
+            }.buttonStyle(CustomButtonStyle())
+            Group {
+                Button(action: {
+                    Identity.getExperienceCloudId { ecid, error in
+                        self.experienceCloudId = ecid ?? ""
+                    }
+                }) {
+                    Text("Get ExperienceCloudId")
+                }.buttonStyle(CustomButtonStyle())
+                VStack {
+                    Text("ExperienceCloudId:")
+                    Text(self.experienceCloudId)
+                }
+            }
+            Group {
+                Button(action: {
+                    MobileCore.getSdkIdentities { identities, _ in
+                        self.sdkIdentities = identities ?? ""
+                    }
+                }) {
+                    Text("Get Sdk Identities")
+                }.buttonStyle(CustomButtonStyle())
+                VStack {
+                    Text("SDK Identities:")
+                    Text(self.sdkIdentities)
+                }
+            }
+            Group {
+                Button(action: {
+                    Identity.getUrlVariables { variables, error in
+                        self.urlVariables = variables ?? ""
+                    }
+                }) {
+                    Text("Get Url Variables")
+                }.buttonStyle(CustomButtonStyle())
+                VStack {
+                    Text("URL Variables:")
+                    Text(self.urlVariables)
+                }
+            }
+            Group {
+                Button(action: {
+                    Identity.appendTo(url: URL(string: "https://example.com")) { url, _ in
+                        self.url = url?.absoluteString ?? ""
+                    }
+                }) {
+                    Text("Append Url")
+                }.buttonStyle(CustomButtonStyle())
+                VStack {
+                    Text("Appended Url")
+                    Text(self.url)
+                }
+            }
+        }
     }
 }
 
