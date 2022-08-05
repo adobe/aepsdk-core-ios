a1642a730fe887759d5150f4b4e5cc09f6406f96
Christopher Hoffmann <chrisdhoff28@gmail.com>
2022-08-05 14:29:01 -0600

    Add identity scrollview and fix broken info.plist for swift test app

--- TestApps/TestApp_Swift/IdentityView.swift
+++ TestApps/TestApp_Swift/IdentityView.swift
@@ -24,75 +24,77 @@ struct IdentityView: View {
 
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            Text("Identity APIs").bold()
-            Button(action: {
-                MobileCore.setAdvertisingIdentifier("advertisingIdentifier")
-            }) {
-                Text("Set Advertising Identifier")
-            }.buttonStyle(CustomButtonStyle())
+        ScrollView {
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Identity APIs").bold()
+                Button(action: {
+                    MobileCore.setAdvertisingIdentifier("advertisingIdentifier")
+                }) {
+                    Text("Set Advertising Identifier")
+                }.buttonStyle(CustomButtonStyle())
 
-            Button(action: {
-                MobileCore.setPushIdentifier("9516258b6230afdd93cf0cd07b8dd845".data(using: .utf8))
-            }) {
-                Text("Set Push Identifier")
-            }.buttonStyle(CustomButtonStyle())
+                Button(action: {
+                    MobileCore.setPushIdentifier("9516258b6230afdd93cf0cd07b8dd845".data(using: .utf8))
+                }) {
+                    Text("Set Push Identifier")
+                }.buttonStyle(CustomButtonStyle())
 
-            Button(action: {
-                Identity.syncIdentifiers(identifiers: ["idType1":"1234567"], authenticationState: .authenticated)
-            }) {
-                Text("Sync Identifiers")
-            }.buttonStyle(CustomButtonStyle())
-            Group {
                 Button(action: {
-                    Identity.getExperienceCloudId { ecid, error in
-                        self.experienceCloudId = ecid ?? ""
-                    }
+                    Identity.syncIdentifiers(identifiers: ["idType1":"1234567"], authenticationState: .authenticated)
                 }) {
-                    Text("Get ExperienceCloudId")
+                    Text("Sync Identifiers")
                 }.buttonStyle(CustomButtonStyle())
-                VStack {
-                    Text("ExperienceCloudId:")
-                    Text(self.experienceCloudId)
+                Group {
+                    Button(action: {
+                        Identity.getExperienceCloudId { ecid, error in
+                            self.experienceCloudId = ecid ?? ""
+                        }
+                    }) {
+                        Text("Get ExperienceCloudId")
+                    }.buttonStyle(CustomButtonStyle())
+                    VStack {
+                        Text("ExperienceCloudId:")
+                        Text(self.experienceCloudId)
+                    }
                 }
-            }
-            Group {
-                Button(action: {
-                    MobileCore.getSdkIdentities { identities, _ in
-                        self.sdkIdentities = identities ?? ""
+                Group {
+                    Button(action: {
+                        MobileCore.getSdkIdentities { identities, _ in
+                            self.sdkIdentities = identities ?? ""
+                        }
+                    }) {
+                        Text("Get Sdk Identities")
+                    }.buttonStyle(CustomButtonStyle())
+                    VStack {
+                        Text("SDK Identities:")
+                        Text(self.sdkIdentities)
                     }
-                }) {
-                    Text("Get Sdk Identities")
-                }.buttonStyle(CustomButtonStyle())
-                VStack {
-                    Text("SDK Identities:")
-                    Text(self.sdkIdentities)
                 }
-            }
-            Group {
-                Button(action: {
-                    Identity.getUrlVariables { variables, error in
-                        self.urlVariables = variables ?? ""
+                Group {
+                    Button(action: {
+                        Identity.getUrlVariables { variables, error in
+                            self.urlVariables = variables ?? ""
+                        }
+                    }) {
+                        Text("Get Url Variables")
+                    }.buttonStyle(CustomButtonStyle())
+                    VStack {
+                        Text("URL Variables:")
+                        Text(self.urlVariables)
                     }
-                }) {
-                    Text("Get Url Variables")
-                }.buttonStyle(CustomButtonStyle())
-                VStack {
-                    Text("URL Variables:")
-                    Text(self.urlVariables)
                 }
-            }
-            Group {
-                Button(action: {
-                    Identity.appendTo(url: URL(string: "https://example.com")) { url, _ in
-                        self.url = url?.absoluteString ?? ""
+                Group {
+                    Button(action: {
+                        Identity.appendTo(url: URL(string: "https://example.com")) { url, _ in
+                            self.url = url?.absoluteString ?? ""
+                        }
+                    }) {
+                        Text("Append Url")
+                    }.buttonStyle(CustomButtonStyle())
+                    VStack {
+                        Text("Appended Url")
+                        Text(self.url)
                     }
-                }) {
-                    Text("Append Url")
-                }.buttonStyle(CustomButtonStyle())
-                VStack {
-                    Text("Appended Url")
-                    Text(self.url)
                 }
             }
         }
