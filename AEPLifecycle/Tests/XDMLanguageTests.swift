1adeed3e82e8e1f8910b6a8135654faab7d9a1b6
Kevin Lind <lind@adobe.com>
2022-12-15 23:01:32 -0800

    Fix Swiftformat

--- AEPLifecycle/Tests/XDMLanguageTests.swift
+++ AEPLifecycle/Tests/XDMLanguageTests.swift
@@ -18,7 +18,7 @@ class XDMLanguageTests: XCTestCase {
     override func setUp() {
         continueAfterFailure = true
     }
-    
+
     // MARK: Encodable tests
 
     func testEncodeLanguage_whenValid() throws {
@@ -41,7 +41,7 @@ class XDMLanguageTests: XCTestCase {
 
         XCTAssertEqual(expected, dataStr)
     }
-    
+
     func testEncodeLanguage_whenInvalid() throws {
         // setup
         let lang = XDMLanguage(language: "en-US@calendar=buddhist")
@@ -56,54 +56,54 @@ class XDMLanguageTests: XCTestCase {
         // verify
         let expected = """
         {
-        
+
         }
         """
 
         XCTAssertEqual(expected, dataStr)
     }
-    
+
     // MARK: isValidLanguageTag tests
-    
+
     // List of test langauge strings to verify isValidLanguageTag function
     // [$0: language string to test, $1: expected is valid]
     let isValidLanguageTagCases = [
-    ("en-US", true),
-    ("en", true),
-    ("und-US", true),
-    ("und-u-va-posix", true),
-    ("und-POSIX", true),
-    ("und", true),
-    ("de-u-va-posix", true),
-    ("de-DE-u-va-posix", true),
-    ("de-DE-POSIX", true),
-    ("de-POSIX", true),
-    ("de-Latn-POSIX", true),
-    ("zh-Hant-HK", true),
-    ("und-Hans-CN", true),
-    ("no-NO-x-lvariant-ny", true),
-    ("sr-Latn-ME", true),
-    ("sr-ME-x-lvariant-Latn", true),
-    ("ja-JP-x-lvariant-jp", true),
-    ("ja-JP-u-ca-japanese", true),
-    ("th-TH-x-lvariant-th", true),
-    ("th-TH-u-ca-buddhist", true),
-    ("en-u-ca-buddhist-nu-thai", true),
-    ("th-TH-u-ca-buddhist-nu-thai", true),
-    ("i-klingon", true),
-    ("en@calendar=buddhist;numbers=thai", false),
-    ("en-US@calendar=buddhist", false),
-    ("-US", false),
-    ("en_US", false),
-    ("de--POSIX", false),
-    ("zh-HK#Hant", false),
-    ("th-TH-TH-#u-nu-thai", false),
-    ("", false)
+        ("en-US", true),
+        ("en", true),
+        ("und-US", true),
+        ("und-u-va-posix", true),
+        ("und-POSIX", true),
+        ("und", true),
+        ("de-u-va-posix", true),
+        ("de-DE-u-va-posix", true),
+        ("de-DE-POSIX", true),
+        ("de-POSIX", true),
+        ("de-Latn-POSIX", true),
+        ("zh-Hant-HK", true),
+        ("und-Hans-CN", true),
+        ("no-NO-x-lvariant-ny", true),
+        ("sr-Latn-ME", true),
+        ("sr-ME-x-lvariant-Latn", true),
+        ("ja-JP-x-lvariant-jp", true),
+        ("ja-JP-u-ca-japanese", true),
+        ("th-TH-x-lvariant-th", true),
+        ("th-TH-u-ca-buddhist", true),
+        ("en-u-ca-buddhist-nu-thai", true),
+        ("th-TH-u-ca-buddhist-nu-thai", true),
+        ("i-klingon", true),
+        ("en@calendar=buddhist;numbers=thai", false),
+        ("en-US@calendar=buddhist", false),
+        ("-US", false),
+        ("en_US", false),
+        ("de--POSIX", false),
+        ("zh-HK#Hant", false),
+        ("th-TH-TH-#u-nu-thai", false),
+        ("", false)
     ]
-    
+
     func testIsValidLanguageTag() throws {
         XCTAssertFalse(isValidLanguageTagCases.isEmpty)
-        
+
         isValidLanguageTagCases.forEach {
             let lang = XDMLanguage(language: $0)
             if $1 {
@@ -112,7 +112,7 @@ class XDMLanguageTests: XCTestCase {
                 XCTAssertNil(lang.language, "Expected input language '\($0)' to be invalid.")
             }
         }
-        
+
 
     }
 
