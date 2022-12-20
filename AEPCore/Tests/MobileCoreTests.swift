a6198273d01b05e992493964360fa9d4666901ab
Kevin Lind <lind@adobe.com>
2022-12-19 22:01:17 -0800

    bump version to 3.7.4

--- AEPCore/Tests/MobileCoreTests.swift
+++ AEPCore/Tests/MobileCoreTests.swift
@@ -207,7 +207,7 @@ class MobileCoreTests: XCTestCase {
               "friendlyName" : "mockExtension"
             },
             "com.adobe.module.configuration" : {
-              "version" : "3.7.3",
+              "version" : "3.7.4",
               "friendlyName" : "Configuration"
             },
             "com.adobe.mockExtensionTwo" : {
