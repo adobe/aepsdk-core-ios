080dd9144c5e6f7d2ff1424b46e410545f5b6f53
Steve Benedick <sbenedic@adobe.com>
2023-03-13 17:08:02 -0600

    Update test-SPM.sh

--- Script/test-SPM.sh
+++ Script/test-SPM.sh
@@ -46,6 +46,12 @@ let package = Package(
 " >Package.swift
 
 swift package update
+swift package resolve
+
+# This is nececery to avoid internal PIF error
+swift package dump-pif > /dev/null
+xcodebuild clean -scheme TestProject -destination 'generic/platform=iOS' > /dev/null
+xcodebuild clean -scheme TestProject -destination 'generic/platform=tvOS' > /dev/null
 
 # Archive for generic iOS device
 echo '############# Archive for generic iOS device ###############'
