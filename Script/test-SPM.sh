01836adcba366dbf13f8029b71b33f8161d7d2d1
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2024-02-05 14:02:55 -0700

    use 5.3 tools version in SPM script

--- Script/test-SPM.sh
+++ Script/test-SPM.sh
@@ -13,7 +13,7 @@ mkdir -p $PROJECT_NAME && cd $PROJECT_NAME
 swift package init
 
 # Create the Package.swift.
-echo "// swift-tools-version:5.1
+echo "// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let package = Package(
