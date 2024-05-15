43c6ffe0229725d45385308d5204c677894b9710
Arjun Bhadra <abhadra@adobe.com>
2024-05-15 11:43:49 -0700

    Update constant name for list of recoverable url error codes

--- AEPServices/Sources/network/NetworkServiceConstants.swift
+++ AEPServices/Sources/network/NetworkServiceConstants.swift
@@ -27,7 +27,7 @@ public struct NetworkServiceConstants {
         public static let CONTENT_TYPE_URL_ENCODED = "application/x-www-form-urlencoded"
     }
     
-    public static let RECOVERABLE_URL_ERRORS = [
+    public static let RECOVERABLE_URL_ERROR_CODES = [
         URLError.timedOut,
         URLError.cannotConnectToHost,
         URLError.networkConnectionLost,
