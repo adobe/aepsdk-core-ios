5b3c2589fbb5e594d67dda7aab5d4e42cb0f4ed5
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2024-06-19 16:16:34 -0600

    further clean up

--- AEPCore/Sources/core/Event+Debug.swift
+++ AEPCore/Sources/core/Event+Debug.swift
@@ -24,18 +24,12 @@ public extension Event {
     
     /// The debug event type (identified by debug.eventType) found in the event data if present, nil otherwise
     var debugEventType: String? {
-        if let debugType = debugEventData?[Event.DEBUG_EVENT_TYPE_KEY] as? String {
-            return debugType
-        }
-        return nil
+        debugEventData?[Event.DEBUG_EVENT_TYPE_KEY] as? String
     }
     
     /// The debug event source (identified by debug.eventSource) found in the event data if present, nil otherwise
     var debugEventSource: String? {
-        if let debugSource = debugEventData?[Event.DEBUG_EVENT_SOURCE_KEY] as? String {
-            return debugSource
-        }
-        return nil
+        debugEventData?[Event.DEBUG_EVENT_SOURCE_KEY] as? String
     }
     
     /// The data found in the event if the event is a debug event, otherwise nil
