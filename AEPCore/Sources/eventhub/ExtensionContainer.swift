7178501436df807bcd6affda0e26002a3d5bcaba
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2024-06-21 11:32:42 -0600

    Add extension name to operation orderer tag for debugging

--- AEPCore/Sources/eventhub/ExtensionContainer.swift
+++ AEPCore/Sources/eventhub/ExtensionContainer.swift
@@ -57,7 +57,7 @@ class ExtensionContainer {
     init(_ name: String, _ type: Extension.Type, _ queue: DispatchQueue, completion: @escaping (EventHubError?) -> Void) {
         extensionQueue = queue
         containerQueue = DispatchQueue(label: "\(name).containerqueue")
-        eventOrderer = OperationOrderer<Event>()
+        eventOrderer = OperationOrderer<Event>(String(describing: type))
         eventListeners = ThreadSafeArray<EventListenerContainer>()
         eventOrderer.setHandler(eventProcessor)
 
