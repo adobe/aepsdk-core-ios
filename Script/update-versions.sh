a6679edb4e79ecd2216ec483c2ae4ff5dd2b2db7
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2024-07-09 14:15:53 -0700

    Consolidate version update script, adding logic to handle TestUtils specific flow

--- Script/update-versions.sh
+++ Script/update-versions.sh
@@ -10,6 +10,13 @@ LINE="==========================================================================
 VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+"
 DEPENDENCIES=none
 
+# Determine the platform (macOS or Linux) to adjust the sed command accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_COMMAND="sed -i '' -E"
+else
+    SED_COMMAND="sed -i -E"
+fi
+
 # make a "dictionary" to help us find the correct spm repo per dependency (if necessary)
 # IMPORTANT - this will be used in a regex search so escape special chars
 # usage :
@@ -64,7 +71,7 @@ echo "$LINE"
 
 # Replace extension version in podspec
 echo "Changing value of 's.version' to '$NEW_VERSION' in '$PODSPEC_FILE'"
-sed -i '' -E "/^ *s.version/{s/$VERSION_REGEX/$NEW_VERSION/;}" $PODSPEC_FILE
+$SED_COMMAND "/^ *s.version/{s/$VERSION_REGEX/$NEW_VERSION/;}" $PODSPEC_FILE
 
 # Replace dependencies in podspec and Package.swift
 if [ "$DEPENDENCIES" != "none" ]; then
@@ -78,12 +85,17 @@ if [ "$DEPENDENCIES" != "none" ]; then
         dependencyVersion=${dependencyArray[1]}
         
         echo "Changing value of 's.dependency' for '$dependencyName' to '>= $dependencyVersion' in '$PODSPEC_FILE'"
-        sed -i '' -E "/^ *s.dependency +'$dependencyName'/{s/$VERSION_REGEX/$dependencyVersion/;}" $PODSPEC_FILE
+        $SED_COMMAND "/^ *s.dependency +'$dependencyName'/{s/$VERSION_REGEX/$dependencyVersion/;}" $PODSPEC_FILE
+
+        # Check if NAME is TestUtils and continue if true - TestUtils does not currently support SPM
+        if [ "$NAME" == "TestUtils" ]; then 
+            continue
+        fi
 
         spmRepoUrl=$(getRepo $dependencyName)
         if [ "$spmRepoUrl" != "" ]; then
             echo "Changing value of '.upToNextMajor(from:)' for '$spmRepoUrl' to '$dependencyVersion' in '$SPM_FILE'"
-            sed -i '' -E "/$spmRepoUrl\", \.upToNextMajor/{s/$VERSION_REGEX/$dependencyVersion/;}" $SPM_FILE
+            $SED_COMMAND "/$spmRepoUrl\", \.upToNextMajor/{s/$VERSION_REGEX/$dependencyVersion/;}" $SPM_FILE
         fi
     done
 fi
@@ -92,30 +104,39 @@ fi
 if [ "$NAME" == "Services" ]; then
     # Nothing, Services has no constants file
     echo "No constants to replace"
+elif [ "$NAME" == "TestUtils" ]; then 
+    # Nothing, TestUtils has no constants file
+    echo "No constants to replace"
 elif [ "$NAME" == "Core" ]; then
     # Core needs to update Event Hub and Configuration Constants
     CONSTANTS_FILE=$ROOT_DIR"/AEP$NAME/Sources/configuration/ConfigurationConstants.swift"
     echo "Changing value of 'EXTENSION_VERSION' to '$NEW_VERSION' in '$CONSTANTS_FILE'"
-    sed -i '' -E "/^ +static let EXTENSION_VERSION/{s/$VERSION_REGEX/$NEW_VERSION/;}" $CONSTANTS_FILE
+    $SED_COMMAND "/^ +static let EXTENSION_VERSION/{s/$VERSION_REGEX/$NEW_VERSION/;}" $CONSTANTS_FILE
 
     EVENT_HUB_FILE=$ROOT_DIR"/AEP$NAME/Sources/eventhub/EventHubConstants.swift"
     echo "Changing value of 'VERSION_NUMBER' to '$NEW_VERSION' in '$EVENT_HUB_FILE'"
-    sed -i '' -E "/^ +static let VERSION_NUMBER/{s/$VERSION_REGEX/$NEW_VERSION/;}" $EVENT_HUB_FILE
+    $SED_COMMAND "/^ +static let VERSION_NUMBER/{s/$VERSION_REGEX/$NEW_VERSION/;}" $EVENT_HUB_FILE
 else
     CONSTANTS_FILE=$ROOT_DIR"/AEP$NAME/Sources/"$NAME"Constants.swift"
     echo "Changing value of 'EXTENSION_VERSION' to '$NEW_VERSION' in '$CONSTANTS_FILE'"
-    sed -i '' -E "/^ +static let EXTENSION_VERSION/{s/$VERSION_REGEX/$NEW_VERSION/;}" $CONSTANTS_FILE
+    $SED_COMMAND "/^ +static let EXTENSION_VERSION/{s/$VERSION_REGEX/$NEW_VERSION/;}" $CONSTANTS_FILE
 fi
 
 # Replace test version in MobileCoreTests
 if [ "$NAME" == "Core" ]; then
     TEST_FILE=$ROOT_DIR"/AEP$NAME/Tests/MobileCoreTests.swift"
     echo "Changing value of 'version' to '$NEW_VERSION' in '$TEST_FILE'"
-    sed -i '' -E "/^ +\"version\" : \"/{s/[1-9]+\.[0-9]+\.[0-9]+/$NEW_VERSION/;}" $TEST_FILE
+    $SED_COMMAND "/^ +\"version\" : \"/{s/[1-9]+\.[0-9]+\.[0-9]+/$NEW_VERSION/;}" $TEST_FILE
+fi
+
+# Early exit if the name is TestUtils
+if [ "$NAME" == "TestUtils" ]; then
+    echo "TestUtils version is not matched with Core. Exiting."
+    exit 0
 fi
 
 # Replace marketing versions in project.pbxproj
 #PROJECT_PBX_FILE=$ROOT_DIR"/AEP$NAME.xcodeproj/project.pbxproj"
 PROJECT_PBX_FILE="$ROOT_DIR/AEPCore.xcodeproj/project.pbxproj"
 echo "Changing value of 'MARKETING_VERSION' to '$NEW_VERSION' in '$PROJECT_PBX_FILE'"
-sed -i '' -E "/^\t+MARKETING_VERSION = /{s/$VERSION_REGEX/$NEW_VERSION/;}" $PROJECT_PBX_FILE
+$SED_COMMAND "/^\t+MARKETING_VERSION = /{s/$VERSION_REGEX/$NEW_VERSION/;}" $PROJECT_PBX_FILE
