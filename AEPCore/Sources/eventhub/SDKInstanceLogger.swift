45638c5bedb66ff7b9b2eeae6ec992cbd64ea71d
Kevin Lind <40409666+kevinlind@users.noreply.github.com>
2024-09-11 10:57:52 -0700

    Add Logger protocol and instance and non-instance implementations. (#1070)
    * Add Logger protocol and instance and non-instance implementations.
    
    * Add DispatchQueue to syncronize access to loggers dictionary.
    
    * Remove exta comment at top of copyright.
    
    * Delete LoggerImpl.swift class as it is not needed right now.
    
    * Remove objc maco from Logger as this class is not public.
    
    * Update dispatch queue label to be consistent with other uses.
    
    * Add objc annotation to Logger
    
    * Rename Loggger getter to 'getInstance(for:)'

---
+++ AEPCore/Sources/eventhub/SDKInstanceLogger.swift
@@ -0,0 +1,73 @@
+/*
+ Copyright 2024 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+*/
+
+import AEPServices
+
+/// Tenant aware logging service.
+class SDKInstanceLogger: Logger {
+    private static var loggerInstances: [SDKInstanceIdentifier: SDKInstanceLogger] = [:]
+    // DispatchQueue to synchronize access to `loggerInstances` dictionary
+    private static let loggerQueue = DispatchQueue(label: "com.adobe.sdkInstanceLogger")
+
+    private let identifier: SDKInstanceIdentifier
+
+    private init(identifier: SDKInstanceIdentifier) {
+        self.identifier = identifier
+    }
+    
+    /// Get a `SDKInstanceLogger` for the given `SDKInstanceIdentifier`.
+    /// - Parameter identifier: the SDK instance identifier.
+    /// - Returns: a `Logger` for the given `instance`.
+    static func getInstance(for identifier: SDKInstanceIdentifier) -> SDKInstanceLogger {
+        loggerQueue.sync {
+            if let logger = loggerInstances[identifier] {
+                return logger
+            } else {
+                let logger = SDKInstanceLogger(identifier: identifier)
+                loggerInstances[identifier] = logger
+                return logger
+            }
+        }
+    }
+
+    /// Used to print more verbose information.
+    /// - Parameters:
+    ///   - label: the name of the label to localize message
+    ///   - message: the string to be logged
+    func trace(label: String, _ message: String) {
+        Log.trace(label: label.instanceAwareName(for: identifier), message)
+    }
+
+    /// Information provided to the debug method should contain high-level details about the data being processed
+    /// - Parameters:
+    ///   - label: the name of the label to localize message
+    ///   - message: the string to be logged
+    func debug(label: String, _ message: String) {
+        Log.debug(label: label.instanceAwareName(for: identifier), message)
+    }
+
+    /// Information provided to the warning method indicates that a request has been made to the SDK, but the SDK will be unable to perform the requested task
+    /// - Parameters:
+    ///   - label: the name of the label to localize message
+    ///   - message: the string to be logged
+    func warning(label: String, _ message: String) {
+        Log.warning(label: label.instanceAwareName(for: identifier), message)
+    }
+
+    /// Information provided to the error method indicates that there has been an unrecoverable error
+    /// - Parameters:
+    ///   - label: the name of the label to localize message
+    ///   - message: the string to be logged
+    func error(label: String, _ message: String) {
+        Log.error(label: label.instanceAwareName(for: identifier), message)
+    }
+}
